b2d03ba4adf59d372a62ff1f77de00c82e9b1a33
omedeiro <omedeiro@mit.edu>
2024-08-02 13:59:29 -0400

    update cells.py

--- src/nmem/measurement/cells.py
+++ src/nmem/measurement/cells.py
@@ -1,20 +1,20 @@
 CELLS = {
     "A1": {
-        "write_current": 320e-6,
-        "read_current": 375e-6,
-        "enable_write_current": 285e-6,
-        "enable_read_current": 285e-6,
+        "write_current": 150e-6,
+        "read_current": 580e-6,
+        "enable_write_current": 335e-6,
+        "enable_read_current": 110e-6,
     },
     "B2": {
-        "write_current": 320e-6,
-        "read_current": 375e-6,
-        "enable_write_current": 315e-6,
-        "enable_read_current": 285e-6,
+        "write_current": 226e-6,
+        "read_current": 480e-6,
+        "enable_write_current": 350e-6,
+        "enable_read_current": 250e-6,
     },
     "C3": {
-        "write_current": 105e-6,
-        "read_current": 436e-6,
-        "enable_write_current": 343e-6,
-        "enable_read_current": 285e-6,
+        "write_current": 390e-6,
+        "read_current": 500e-6,
+        "enable_write_current": 246e-6,
+        "enable_read_current": 200e-6,
     },
 }
