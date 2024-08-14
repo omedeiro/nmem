371bb23fc096f8a6b577986d45ac046e4f489342
omedeiro <omedeiro@mit.edu>
2024-08-14 09:16:06 -0400

    update find peak and plotting

--- src/nmem/analysis/enable_current_relation.py
+++ src/nmem/analysis/enable_current_relation.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
-from matplotlib.axes import Axes
-
+import os
 
 def find_peak(data: dict):
     x = data["x"][0][:, 1] * 1e6
@@ -16,20 +15,21 @@ def find_peak(data: dict):
     dx = x[1] - x[0]
     dy = y[1] - y[0]
 
-    mid = np.where((ztotal > 10) * (ztotal < 190), ztotal, 0)
-    mid_idx = np.where(mid > 0)
+    # Find the maximum critical current using np.diff
+    diff = np.diff(ztotal, axis=0)
+    mid_idx = np.where(diff == np.max(diff, axis=0))
 
     plt.scatter(x[mid_idx[1]], y[mid_idx[0]])
 
     # Plot a fit line to the scatter points
-    z = np.polyfit(x[mid_idx[1]], y[mid_idx[0]], 1)
+    z = np.polyfit(x[mid_idx[1][4:-4]], y[mid_idx[0][4:-4]], 1)
     p = np.poly1d(z)
 
     print(f"slope: {z[0]}, intercept: {z[1]}")
     plt.plot(x[mid_idx[1]], p(x[mid_idx[1]]), "r--")
 
     plt.imshow(
-        ztotal,
+        diff,
         extent=[
             (-0.5 * dx + x[0]),
             (0.5 * dx + x[-1]),
@@ -41,23 +41,39 @@ def find_peak(data: dict):
     )
     plt.xticks(np.linspace(x[0], x[-1], len(x)))
     plt.yticks(np.linspace(y[0], y[-1], len(y)))
+    plt.xlabel("Enable Current [$\mu$A]")
+    plt.ylabel("Channel Current [$\mu$A]")
+    plt.title(f"Cell {data['cell']}")
     cbar = plt.colorbar()
 
     # Add fit slope and intercept to the plot
     print(p)
     plt.text(x[0], y[0], f"{p}", fontsize=12, color="red", backgroundcolor="white")
-    return z
+    return ztotal
+
+def find_max_critical_current(data):
+    x = data["x"][0][:, 1] * 1e6
+    y = data["y"][0][:, 0] * 1e6
+    w0r1 = 100 - data["write_0_read_1"][0].flatten()
+    w1r0 = data["write_1_read_0"][0].flatten()
+    z = w1r0 + w0r1
+    ztotal = z.reshape((len(y), len(x)), order="F")
+    ztotal = ztotal[:,1]
+    
+    # Find the maximum critical current using np.diff
+    diff = np.diff(ztotal)
+    mid_idx = np.where(diff == np.max(diff))
+
+    return np.mean(y[mid_idx])
 
 
 if __name__ == "__main__":
-    # data = "data\SPG806_20240804_measure_enable_response_D6_A4_2024-08-04 18-59-58.mat"
-    data = "data\SPG806_20240806_measure_enable_response_D6_A4_B1_2024-08-06 17-30-39.mat"
-# C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\data\SPG806_20240806_measure_enable_response_D6_A4_B3_2024-08-06 16-22-06.mat
-    enable_current = 0.0
-    # enable_current_relation(data, enable_current)
-    data_dict = sio.loadmat(data)
-    find_peak(data_dict)
-
-    for key in data_dict.keys():
-        if isinstance(data_dict[key], np.ndarray):
-            print(f"key: {key}, shape: {data_dict[key].shape}")
+    files = os.listdir("data")
+    for file in files:
+        data = os.path.join("data", file)
+        cell = file.split("_")[-2]
+        data_dict = sio.loadmat(data)
+        data_dict["cell"] = cell
+        ztotal = find_peak(data_dict)
+        plt.show()
+        print(find_max_critical_current(data_dict))
