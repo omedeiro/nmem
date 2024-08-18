42c774b1b8ad742b5238f4ef145411b1378c6ca1
omedeiro <omedeiro@mit.edu>
2024-08-18 15:41:28 -0400

    update plotting

--- src/nmem/analysis/enable_current_relation.py
+++ src/nmem/analysis/enable_current_relation.py
@@ -99,41 +99,96 @@ def plot_fit(xfit, yfit):
     )
 
 
-if __name__ == "__main__":
-    files = os.listdir("data")
-    markers = ["o", "s", "D", "^"]
-    colors = plt.cm.viridis(np.linspace(0, 1, 5))
-    colors = np.flipud(colors)
-    for file in [files[-5]]:
-        datafile = os.path.join("data", file)
-        cell = file.split("_")[-2]
-        column = ord(cell[0]) - ord("A") - 1
-        row = int(cell[1]) - 1
+def plot_enable_current_relation(data_dict: dict):
+    x = data_dict["x"][0][:, 1] * 1e6
+    y = data_dict["y"][0][:, 0] * 1e6
 
-        measurement_dict = sio.loadmat(datafile)
-        measurement_dict["cell"] = cell
-        xfit, yfit = find_enable_relation(measurement_dict)
-        xfit_sorted = np.sort(xfit)
-        yfit_sorted = yfit[np.argsort(xfit)]
+    w0r1 = 100 - data_dict["write_0_read_1"][0].flatten()
+    w1r0 = data_dict["write_1_read_0"][0].flatten()
+    z = w1r0 + w0r1
+    ztotal = z.reshape((len(y), len(x)), order="F")
 
-        plt.plot(
-            xfit_sorted,
-            yfit_sorted,
-            "-",
-            marker=markers[column],
-            color=colors[row+1, :],
-            label=cell,
-        )
-        y = measurement_dict["y"][0][:, 0] * 1e6
-        dy = y[1] - y[0]
-        yerr = np.ones_like(yfit) * dy
-        find_peak(measurement_dict)
-    plt.xticks(np.linspace(200, 300, 3))
-    plt.yticks(np.linspace(300, 900, 3))
+    dx = x[1] - x[0]
+    dy = y[1] - y[0]
+
+    # Find the maximum critical current using np.diff
+    diff = np.diff(ztotal, axis=0)
+
+    xfit, yfit = find_enable_relation(data_dict)
+    # xfit = xfit[:-1]
+    # yfit = yfit[:-1]
+
+    plt.scatter(xfit, yfit)
+
+    # Plot a fit line to the scatter points
+    plot_fit(xfit, yfit)
+
+    plt.imshow(
+        ztotal,
+        extent=[
+            (-0.5 * dx + x[0]),
+            (0.5 * dx + x[-1]),
+            (-0.5 * dy + y[0]),
+            (0.5 * dy + y[-1]),
+        ],
+        aspect="auto",
+        origin="lower",
+        cmap="viridis",
+    )
+    # plt.xticks(np.linspace(200, 300, 3))
+    # plt.yticks(np.linspace(300, 900, 3))
     plt.xlim(180, 320)
     plt.ylim(280, 920)
     plt.xlabel("Enable Current [$\mu$A]")
     plt.ylabel("Channel Current [$\mu$A]")
     plt.title("Enable Current Relation")
     plt.grid()
+    plt.show()
+
+    return ztotal
+
+
+if __name__ == "__main__":
+    files = os.listdir("data")
+    markers = ["o", "s", "D", "^"]
+    colors = plt.cm.viridis(np.linspace(0, 1, 4))
+    # colors = np.flipud(colors)
+    for file in files:
+        datafile = os.path.join("data", file)
+        cell = file.split("_")[-2]
+        column = ord(cell[0]) - ord("A")
+        row = int(cell[1]) - 1
+
+        measurement_dict = sio.loadmat(datafile)
+        measurement_dict["cell"] = cell
+        xfit, yfit = find_enable_relation(measurement_dict)
+        xfit_sorted = np.sort(xfit) * 0.9
+        yfit_sorted = yfit[np.argsort(xfit)] * 0.597
+        print(f"Cell: {cell} max Ic = {yfit_sorted[0]}")
+
+        # plot_enable_current_relation(measurement_dict)
+        # plot_fit(xfit_sorted, yfit_sorted)
+        plt.plot(xfit_sorted, yfit_sorted, label=f"Cell {cell}", marker=markers[row], color=colors[column])
+        # w0r1 = 100 - measurement_dict["write_0_read_1"][0].flatten()
+        # w1r0 = measurement_dict["write_1_read_0"][0].flatten()
+        # z = w1r0 + w0r1
+        # ztotal = z.reshape(
+        #     (measurement_dict["y"][0].shape[0], measurement_dict["x"][0].shape[0]),
+        #     order="F",
+        # )
+        # cmap = plt.cm.viridis(np.linspace(0, 1, ztotal.shape[1]))
+        # for enable_current_index in range(ztotal.shape[1]):
+        #     enable_current = measurement_dict["x"][0][enable_current_index, 1] * 1e6
+        #     plt.plot(
+        #         measurement_dict["y"][0][:, 0]*1e6,
+        #         ztotal[:, enable_current_index],
+        #         label=f"enable current = {enable_current:.2f} $\mu$A",
+        #         color=cmap[enable_current_index],
+        #     )
+    plt.xlabel("Channel Current [$\mu$A]")
+    plt.ylabel("Counts")
+    ax = plt.gca()
+    ax.set_ylim(bottom=0)
+    plt.title(f"Cell {cell}")
+    plt.legend()
     plt.show()
