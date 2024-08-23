86608480e8f1a7bc823843e8d8923f8634d48667
omedeiro <omedeiro@mit.edu>
2024-08-23 15:12:22 -0400

    import config. delete initalize lines for function. param update. fix dict update

--- src/nmem/measurement/parameter_sweep.py
+++ src/nmem/measurement/parameter_sweep.py
@@ -16,6 +16,7 @@ import nmem.measurement.functions as nm
 from nmem.calculations.calculations import calculate_critical_current
 from nmem.measurement.cells import (
     CELLS,
+    CONFIG,
     FREQ_IDX,
     HEATERS,
     HORIZONTAL_SCALE,
@@ -28,37 +29,11 @@ from nmem.measurement.cells import (
 
 plt.rcParams["figure.figsize"] = [10, 12]
 
-CONFIG = r"SPG806_config_ICE.yml"
-
 
 if __name__ == "__main__":
     t1 = time.time()
-    b = nt.nTron(CONFIG)
-
-    b.inst.awg.write("SOURce1:FUNCtion:ARBitrary:FILTer OFF")
-    b.inst.awg.write("SOURce2:FUNCtion:ARBitrary:FILTer OFF")
-
-    current_cell = b.properties["Save File"]["cell"]
-    sample_name = [
-        b.sample_name,
-        b.device_type,
-        b.device_name,
-        current_cell,
-    ]
-    sample_name = str("-".join(sample_name))
-    date_str = time.strftime("%Y%m%d")
-    measurement_name = f"{date_str}_nMem_ICE_ber"
-
-    current_cell = b.properties["Save File"]["cell"]
-    sample_name = [
-        b.sample_name,
-        b.device_type,
-        b.device_name,
-        current_cell,
-    ]
-    sample_name = str("-".join(sample_name))
-    date_str = time.strftime("%Y%m%d")
-    measurement_name = f"{date_str}_nMem_ICE_ber"
+    measurement_name = "nMem_parameter_sweep"
+    measurement_settings, b = nm.initilize_measurement(CONFIG, measurement_name)
 
     waveform_settings = {
         "num_points": NUM_POINTS,
@@ -74,10 +49,10 @@ if __name__ == "__main__":
     }
 
     current_settings = {
-        "write_current": 180e-6,
-        "read_current": 478e-6,
-        "enable_write_current": 265e-6,
-        "enable_read_current": 240e-6,
+        "write_current": 20.002e-6,
+        "read_current": 487.976e-6,
+        "enable_write_current": 249.494e-6,
+        "enable_read_current": 182.499e-6,
     }
 
     scope_settings = {
@@ -86,45 +61,43 @@ if __name__ == "__main__":
         "scope_num_samples": NUM_SAMPLES,
         "scope_sample_rate": NUM_SAMPLES / (HORIZONTAL_SCALE[FREQ_IDX] * NUM_DIVISIONS),
     }
-    NUM_MEAS = 100
+    NUM_MEAS = 5000
 
-    measurement_settings = {
-        **waveform_settings,
-        **current_settings,
-        **scope_settings,
-        "measurement_name": measurement_name,
-        "sample_name": sample_name,
-        "cell": current_cell,
-        "CELLS": CELLS,
-        "HEATERS": HEATERS,
-        "num_meas": NUM_MEAS,
-        "spice_device_current": SPICE_DEVICE_CURRENT,
-        "x": 0,
-        "y": 0,
-        "threshold_bert": 0.2,
-    }
-
-    parameter_x = "enable_read_current"
-    measurement_settings["x"] = np.array([240e-6])
-    # measurement_settings["x"] = np.linspace(190e-6, 200e-6, 11)
+    measurement_settings.update(
+        {
+            **waveform_settings,
+            **current_settings,
+            **scope_settings,
+            "CELLS": CELLS,
+            "HEATERS": HEATERS,
+            "num_meas": NUM_MEAS,
+            "spice_device_current": SPICE_DEVICE_CURRENT,
+            "x": 0,
+            "y": 0,
+            "threshold_bert": 0.2,
+        }
+    )
+    parameter_x = "enable_write_current"
+    measurement_settings["x"] = np.array([249.494e-6])
+    # measurement_settings["x"] = np.linspace(260e-6, 290e-6, 5)
     measurement_settings[parameter_x] = measurement_settings["x"][0]
     # measurement_settings["x"] = np.linspace(260e-6, 290e-6, 3)
 
     read_sweep = True
     if read_sweep:
         parameter_y = "read_current"
-        measurement_settings["y"] = np.array([478e-6])
-        # measurement_settings["y"] = np.linspace(610e-6, 640e-6, 11)
+        measurement_settings["y"] = np.array([487.976e-6])
+        # measurement_settings["y"] = np.linspace(680e-6, 760e-6, 11)
         # read_critical_current = calculate_critical_current(
         #     measurement_settings["enable_read_current"], CELLS[current_cell]
         # )
         # measurement_settings["y"] = np.linspace(
-        #     read_critical_current * 0.7, read_critical_current * 0.85, 11
+        #     read_critical_current * 0.6, read_critical_current * 0.7, 11
         # )
     else:
         parameter_y = "write_current"
-        # measurement_settings["y"] = np.array([88.61e-6])
-        measurement_settings["y"] = np.linspace(40e-6, 90e-6, 11)
+        measurement_settings["y"] = np.array([30e-6])
+        # measurement_settings["y"] = np.linspace(40e-6, 90e-6, 11)
         # write_critical_current = calculate_critical_current(
         #     measurement_settings["enable_write_current"], CELLS[current_cell]
         # )
