a05101f506f0c173f511fdb55089aed95093a6b4
omedeiro <omedeiro@mit.edu>
2024-10-03 10:27:51 -0400

    plotting and fitting updates

--- src/nmem/analysis/enable_read_current/enable_read_current_sweep.py
+++ src/nmem/analysis/enable_read_current/enable_read_current_sweep.py
@@ -2,12 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 from matplotlib.collections import PolyCollection
+from matplotlib.ticker import MultipleLocator
 from mpl_toolkits.mplot3d import Axes3D
+from scipy.signal import find_peaks
 
 from nmem.measurement.cells import CELLS
 
 plt.rcParams["figure.figsize"] = [6, 4]
 plt.rcParams["font.size"] = 14
+
+
+CURRENT_CELL = "C1"
 
 
 def polygon_under_graph(x, y):
@@ -35,6 +40,7 @@ def plot_waterfall(data_dict: dict, ax: Axes3D = None):
         zlist.append(enable_read_current)
         verts = polygon_under_graph(read_currents, ber)
         verts_list.append(verts)
+
     poly = PolyCollection(verts_list, facecolors=colors, alpha=0.6, edgecolors="k")
     ax.add_collection3d(poly, zs=zlist, zdir="y")
 
@@ -51,16 +57,46 @@ def plot_waterfall(data_dict: dict, ax: Axes3D = None):
 
     ax.set_zlim(0, 1)
     ax.set_zticks([0, 0.5, 1])
-    ax.set_xlim(590, 950)
+    ax.set_xlim(500, 950)
+    ax.xaxis.set_major_locator(MultipleLocator(100))
     ax.set_ylim(zlist[0], zlist[-1])
-    # ax.set_xticks(np.linspace(300, 600, 4))
-    # ax.set_yticks(zlist)
+    ax.set_yticks(zlist)
     ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
     ax.set_box_aspect([0.5, 1, 0.2], zoom=0.8)
     ax.view_init(20, -35)
     ax.grid(False)
-    ax.grid(axis="x", linestyle=":", color="lightgray")
+
+    for key, data in data_dict.items():
+        colors = ["red", "darkred", "green", "limegreen"]
+        edge_dict = get_edges({key: data})
+        edge_list = edge_dict[key]["edges"]
+        read_currents = data["y"][:, :, 0].flatten() * 1e6
+        current_cell = data["cell"][0]
+        ber = data["bit_error_rate"].flatten()
+        w0r1 = data["write_0_read_1_norm"].flatten()
+        w1r0 = data["write_1_read_0_norm"].flatten()
+        enable_read_current = data["enable_read_current"].flatten()[0] * 1e6
+
+        ax = plot_edge_3D(edge_dict, edge_list, key, colors, ax)
+        peaks = edge_dict[key]["peaks"]
+        peak_properties = edge_dict[key]["peak_properties"]
+        inv_peaks = edge_dict[key]["inv_peaks"]
+        inv_peak_properties = edge_dict[key]["inv_peak_properties"]
+        ax.plot(
+            read_currents,
+            enable_read_current * np.ones_like(read_currents),
+            w0r1,
+            "-",
+            color="r",
+        )
+        ax.plot(
+            read_currents,
+            enable_read_current * np.ones_like(read_currents),
+            w1r0,
+            "-",
+            color="b",
+        )
     ax = plt.gca()
     return ax
 
@@ -71,16 +107,16 @@ def load_data(file_path: str):
 
 
 def find_edge(data: np.ndarray) -> list:
-    pos_data = np.argwhere(data > 0.6)
-    neg_data = np.argwhere(data < 0.4)
+    pos_data = np.argwhere(data > 0.55)
+    neg_data = np.argwhere(data < 0.45)
 
-    if len(pos_data)>0:
+    if len(pos_data) > 0:
         pos_edge1 = pos_data[0][0]
         neg_edge1 = pos_data[-1][0]
     else:
         pos_edge1 = 0
         neg_edge1 = 0
-    if len(neg_data)>0:
+    if len(neg_data) > 0:
         neg_edge2 = neg_data[0][0]
         pos_edge2 = neg_data[-1][0]
     else:
@@ -88,14 +124,176 @@ def find_edge(data: np.ndarray) -> list:
         pos_edge2 = 0
     return [pos_edge1, neg_edge1, neg_edge2, pos_edge2]
 
-def get_edges(data_dict: dict):
-    edges = []
+
+def find_edge_dual(ber: np.ndarray, w0r1: np.ndarray, w1r0: np.ndarray) -> list:
+    w0r1_peak = np.argmax(np.diff(w0r1))
+    w0r1_peak_neg = np.argmin(np.diff(w0r1))
+    w1r0_peak = np.argmax(np.diff(w1r0))
+    w1r0_peak_neg = np.argmin(np.diff(w1r0))
+    left_nominal = np.argmin(np.diff(w1r0))
+    right_nominal = np.argmax(np.diff(w0r1))
+    left_inverting = np.argmax(np.diff(w0r1))
+
+    # if right_nominal == 0:
+    #     right_nominal = np.argmax(np.diff(w1r0[left_nominal:]))
+
+    # if right_nominal < left_nominal:
+    #     right_nominal = np.argmax(np.diff(w0r1[left_nominal:]))
+        
+    # if left_nominal > right_nominal:
+    #     left_nominal = np.argmin(np.diff(w1r0[:left_nominal]))
+        
+    # if right_nominal < left_nominal:
+    #     right_nominal = np.argmax(np.diff(w0r1[left_nominal:]))
+
+    while ber[left_inverting] < 0.5:
+        left_inverting = np.argmax(np.diff(w0r1[:left_nominal]))
+
+    if np.min(ber)>=0.45:
+        left_nominal = 0
+        right_nominal = 0
+    # if left_nominal > right_nominal:
+    #     left_nominal = np.argmin(np.diff(w1r0[:left_nominal]))
+    #     right_nominal = np.argmax(np.diff(ber[left_nominal:]))
+    # right_nominal = np.argmax(np.diff(w0r1[right_nominal:])
+
+    # if ber[w0r1_peak] < 0.45:
+    #     w0r1_peak = np.argmax(np.diff(w0r1[:w0r1_peak]))
+    # if w0r1_peak_neg > w0r1_peak:
+    #     w0r1_peak = np.argmax(np.diff(w0r1[w0r1_peak_neg:]))
+    # if w1r0_peak_neg > w1r0_peak:
+    #     w1r0_peak_neg = np.argmin(np.diff(w1r0[:w1r0_peak_neg]))
+    print(f"left_nominal: {left_nominal}, right_nominal: {right_nominal}")
+    print(f"left invert: {left_inverting}")
+    return [left_inverting]
+
+
+def get_edges(data_dict: dict) -> dict:
+    edges = {}
     for key, data in data_dict.items():
         read_currents = data["y"][:, :, 0].flatten() * 1e6
         ber = data["bit_error_rate"].flatten()
-        edges.append(find_edge(ber))
+        w0r1 = data["write_0_read_1_norm"].flatten()
+        w1r0 = data["write_1_read_0_norm"].flatten()
+        enable_read_current = data["enable_read_current"].flatten()[0] * 1e6
+        edges[key] = {}
+        edges[key]["edges"] = find_edge(ber)
+        # edges[key]["edges"] = find_edge_dual(ber, w0r1, w1r0)
+        edges[key]["param"] = enable_read_current
+        edges[key]["read_currents"] = read_currents
+        peaks, peak_properties = find_peaks(
+            ber, height=0.4, prominence=0.05, width=1, distance=1
+        )
+        inv_peaks, inv_peak_properties = find_peaks(
+            1 - ber, height=0.4, prominence=0.05, width=1, distance=1
+        )
+        edges[key]["ber"] = ber
+        edges[key]["peaks"] = peaks
+        edges[key]["peak_properties"] = peak_properties
+        edges[key]["inv_peaks"] = inv_peaks
+        edges[key]["inv_peak_properties"] = inv_peak_properties
 
     return edges
+
+
+def plot_edges(data_dict: dict, ax: plt.Axes, fit=True) -> plt.Axes:
+    colors = ["red", "darkred", "green", "limegreen"]
+    edge_dict = get_edges(data_dict)
+    edge_list = []
+    param_list = []
+    if ax is None:
+        fig, ax = plt.subplots()
+    for key, data in edge_dict.items():
+        read_current = data["read_currents"]
+        param = data["param"]
+        param = param * CELLS[CURRENT_CELL]["slope"] + CELLS[CURRENT_CELL]["intercept"]
+        param_list.append(param)
+        edge = data["edges"]
+        current_edge = np.array([read_current[e] for e in edge])
+        current_edge = np.where(np.array(edge) == 0, np.nan, current_edge)
+
+        edge_list.append(current_edge)
+
+        for i, e in enumerate(edge):
+            if e == 0:
+                continue
+            plt.scatter(param, read_current[e], color=colors[i])
+
+    if fit:
+        for i in [1, 3]:
+            x = np.array(param_list)
+            y = [edge[i] for edge in edge_list]
+            y = np.array(y)
+            fity = y[~np.isnan(y)]
+            fitx = x[~np.isnan(y)]
+            fitx = fitx[0:-2]
+            fity = fity[0:-2]
+            fit = np.polyfit(fitx, fity, 1)
+            fit_fn = np.poly1d(fit)
+            plt.plot(x, fit_fn(x), "--k")
+            plt.plot(fitx, fity, "o", color="k", fillstyle="none", mew=1.5)
+            plt.text(
+                0.50,
+                0.4 + 0.1 * i,
+                f"y = {fit_fn[1]:.2f}x + {fit_fn[0]:.2f}",
+                transform=ax.transAxes,
+            )
+    ax = plt.gca()
+    
+    return ax
+
+
+def plot_edge_3D(edge_dict: dict, edge_list: list, key: int, colors: list, ax: Axes3D):
+    for edge in edge_list:
+        if edge == 0:
+            continue
+        ax.plot(
+            [
+                edge_dict[key]["read_currents"][edge],
+                edge_dict[key]["read_currents"][edge],
+            ],
+            [edge_dict[key]["param"], edge_dict[key]["param"]],
+            [0, edge_dict[key]["ber"][edge]],
+            color="k",
+            linestyle=":",
+        )
+        ax.plot(
+            [
+                edge_dict[key]["read_currents"][edge],
+                edge_dict[key]["read_currents"][edge],
+            ],
+            [edge_dict[key]["param"], edge_dict[key]["param"]],
+            [edge_dict[key]["ber"][edge]],
+            color=colors[edge_list.index(edge)],
+            marker="o",
+        )
+
+    return ax
+
+
+def plot_enable_read_current_edges(data_dict: dict):
+    fig, ax = plt.subplots()
+    ax = plot_edges(data_dict, ax)
+
+    enable_write_current = data_dict[0]["enable_write_current"].flatten()[0] * 1e6
+
+    ax.xaxis.set_major_locator(MultipleLocator(50))
+    ax.yaxis.set_major_locator(MultipleLocator(50))
+    plt.text(
+        0.65,
+        0.85,
+        f"$I_{{EW}}$ = {enable_write_current:.1f}$\mu$A",
+        transform=ax.transAxes,
+    )
+
+    plt.ylim(500, 950)
+    # ax.invert_xaxis()
+    plt.title("Enable Read Current Edges")
+    plt.xlabel("Channel Critical Current ($\mu$A)")
+    plt.ylabel("Read Current ($\mu$A)")
+    plt.grid(True)
+    ax.set_aspect("equal")
+    plt.show()
 
 
 def plot_read_sweep_single(data_dict: dict, index: int, ax=None, fill=False):
@@ -119,7 +317,6 @@ def plot_read_sweep_single(data_dict: dict, index: int, ax=None, fill=False):
             marker=".",
             markeredgecolor="k",
         )
-
 
         edges = find_edge(ber)
         for edge in edges:
@@ -207,7 +404,16 @@ def plot_sweep_waterfall(data_dict: dict):
         [["waterfall"]], figsize=(16, 9), subplot_kw={"projection": "3d"}
     )
     ax["waterfall"] = plot_waterfall(data_dict, ax=ax["waterfall"])
-    plt.savefig("enable_write_current_sweep.pdf", format="pdf", bbox_inches="tight")
+    enable_write_current = data_dict[0]["enable_write_current"].flatten()[0] * 1e6
+    # ax["waterfall"].set_title(f"$I_{{EW}}$ = {enable_write_current:.1f}$\mu$A")
+    ax["waterfall"].text2D(
+        0.25,
+        0.6,
+        f"$I_{{EW}}$ = {enable_write_current:.1f}$\mu$A",
+        transform=ax["waterfall"].transAxes,
+    )
+    fig.tight_layout()
+    plt.savefig(f"enable_read_current_sweep_{int(enable_write_current)}.pdf")
     plt.show()
 
 
@@ -239,53 +445,100 @@ if __name__ == "__main__":
 
     data_dict = {0: data0, 1: data1, 2: data2, 3: data3, 4: data4}
 
-    enable_read_long_dict = {
+    enable_read_290_dict_full = {
         0: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-10-30.mat"
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-45-11.mat"
         ),
         1: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-19-12.mat"
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-53-18.mat"
         ),
         2: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-26-55.mat"
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 16-08-51.mat"
         ),
         3: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-33-48.mat"
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 16-19-03.mat"
         ),
         4: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-40-47.mat"
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-10-30.mat"
         ),
         5: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-49-39.mat"
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-19-12.mat"
         ),
         6: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-26-55.mat"
+        ),
+        7: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-33-48.mat"
+        ),
+        8: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-40-47.mat"
+        ),
+        9: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-49-39.mat"
+        ),
+        10: load_data(
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-59-27.mat"
+        ),
+        11: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-10-02.mat"
+        ),
+        12: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-17-53.mat"
+        ),
+        13: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-24-46.mat"
+        ),
+        14: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-32-29.mat"
+        ),
+        15: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-40-00.mat"
+        ),
+        16: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-53-35.mat"
+        ),
+        17: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-02-47.mat"
+        ),
+    }
+
+    enable_read_290_dict = {
+        0: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-45-11.mat"
+        ),
+        1: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-53-18.mat"
+        ),
+        2: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 16-08-51.mat"
+        ),
+        3: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 16-19-03.mat"
+        ),
+        4: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-19-12.mat"
+        ),
+        5: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-33-48.mat"
+        ),
+        6: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-49-39.mat"
         ),
         7: load_data(
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-10-02.mat"
         ),
         8: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-17-53.mat"
-        ),
-        9: load_data(
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-24-46.mat"
         ),
-        10: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-32-29.mat"
-        ),
-        11: load_data(
+        9: load_data(
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-40-00.mat"
         ),
-        12: load_data(
-            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-53-35.mat"
-        ),
-        13: load_data(
+        10: load_data(
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-02-47.mat"
         ),
     }
 
-
-    enable_read_short_dict = {
+    enable_read_290_dict_short = {
         0: load_data(
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 12-19-12.mat"
         ),
@@ -296,9 +549,87 @@ if __name__ == "__main__":
             "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 13-24-46.mat"
         ),
     }
+
+    enable_read_300_dict = {
+        0: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 14-54-06.mat"
+        ),
+        1: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-06-21.mat"
+        ),
+        2: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 14-44-04.mat"
+        ),
+        3: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-16-22.mat"
+        ),
+        4: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-23-31.mat"
+        ),
+        5: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-30-28.mat"
+        ),
+        6: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-39-15.mat"
+        ),
+        7: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-47-05.mat"
+        ),
+        8: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 15-54-14.mat"
+        ),
+        9: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 16-04-36.mat"
+        ),
+        10: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 14-29-17.mat"
+        ),
+    }
+
+    enable_read_310_dict = {
+        0: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-37-34.mat"
+        ),
+        1: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-30-17.mat"
+        ),
+        2: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-21-01.mat"
+        ),
+        3: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-13-38.mat"
+        ),
+        4: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-15-11.mat"
+        ),
+        5: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-27-11.mat"
+        ),
+        6: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-34-04.mat"
+        ),
+        7: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-44-33.mat"
+        ),
+        8: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 14-56-20.mat"
+        ),
+        9: load_data(
+            "SPG806_20240930_nMem_parameter_sweep_D6_A4_C1_2024-09-30 15-05-30.mat"
+        ),
+        10: load_data(
+            "SPG806_20241001_nMem_parameter_sweep_D6_A4_C1_2024-10-01 09-17-27.mat"
+        ),
+    }
     # plot_enable_read_sweep_multiple(data_dict)
     # plot_enable_write_sweep_multiple(write_dict)
 
-    plot_enable_read_sweep_multiple(enable_read_long_dict)
+    # plot_enable_read_sweep_multiple(enable_read_300_dict)
+
+    plot_enable_read_current_edges(enable_read_290_dict)
+    plot_enable_read_current_edges(enable_read_300_dict)
+    plot_enable_read_current_edges(enable_read_310_dict)
+
+    # plot_sweep_waterfall(enable_read_300_dict)
 
     # plot_sweep_waterfall(enable_read_long_dict)
