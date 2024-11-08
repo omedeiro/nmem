714dfbcf697f90deb681eabd9e090f0bf68482c0
omedeiro <omedeiro@mit.edu>
2024-11-08 13:12:37 -0500

    plotting update

--- src/nmem/analysis/main_analysis.py
+++ src/nmem/analysis/main_analysis.py
@@ -25,6 +25,7 @@ plt.rcParams["lines.markersize"] = 2
 plt.rcParams["lines.linewidth"] = 0.5
 plt.rcParams["legend.fontsize"] = 5
 plt.rcParams["legend.frameon"] = False
+plt.rcParams['axes.labelpad'] = 0.5
 
 if __name__ == "__main__":
     xloc = []
@@ -39,6 +40,7 @@ if __name__ == "__main__":
     resistance_array = np.zeros((4, 4))
     bit_error_array = np.empty((4, 4))
     max_critical_current_array = np.zeros((4, 4))
+    max_heater_current = np.zeros((4, 4))
     enable_write_array = np.zeros((4, 4))
     enable_read_array = np.zeros((4, 4))
     enable_write_power_array = np.zeros((4, 4))
@@ -87,10 +89,18 @@ if __name__ == "__main__":
             enable_write_power_array[y, x] = enable_write_power
             enable_read_power_array[y, x] = enable_read_power
 
-    fig, axs = plt.subplot_mosaic([["bit_error", "write", "read"], ["max_critical_current", "fom", "power"]])
+    fig, axs = plt.subplot_mosaic([["bit_error", "max_heater_current", "delay"], ["max_critical_current", "read", "temperature"]])
     plot_array(xloc, yloc, bit_error_array, log=True, ax=axs["bit_error"])
     plot_array(xloc, yloc, max_critical_current_array, log=True, ax=axs["max_critical_current"])
+    plot_array(xloc, yloc, x_intercept_array, log=True, ax=axs["max_heater_current"])
+    plot_array(xloc, yloc, read_array, log=True, ax=axs["read"])
+
+    axs["delay"].set_xlabel("Delay ($\mu$s)")
+    axs["delay"].set_ylabel("BER")
+
+
+    axs["temperature"].set_xlabel("Temperature (K)")
+    axs["temperature"].set_ylabel("BER")
 
 
     plt.savefig("main_analysis.pdf", bbox_inches="tight")
-    
