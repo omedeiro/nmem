6bcc7970fe7e10f7c17d4ceddb85b7c458a037d9
omedeiro <omedeiro@mit.edu>
2024-12-12 10:08:29 -0500

    replace function with generic

--- src/nmem/analysis/write_sweep/write_sweep_v3.py
+++ src/nmem/analysis/write_sweep/write_sweep_v3.py
@@ -1,52 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
-
+from nmem.analysis.analysis import plot_read_sweep_array
 plt.rcParams["figure.figsize"] = [6, 4]
 plt.rcParams["font.size"] = 14
-
-
-def plot_write_sweep_single(data_dict: dict, index: int, ax=None):
-    if ax is None:
-        fig, ax = plt.subplots()
-    plt.sca(ax)
-    cmap = plt.get_cmap("Greens")
-    colors = cmap(np.linspace(0.3, 1, len(data_dict)))
-
-    data_dict = {index: data_dict[index]}
-
-    for key, data in data_dict.items():
-        read_currents = data["y"][:, :, 0].flatten() * 1e6
-        ber = data["bit_error_rate"].flatten()
-        write_current = data["write_current"].flatten()[0] * 1e6
-        plt.plot(
-            read_currents,
-            ber,
-            label=f"$I_W$ = {write_current:.1f}$\mu$A",
-            color=colors[key],
-            marker=".",
-            markeredgecolor="k",
-        )
-
-    ax = plt.gca()
-    plt.xlim(570, 650)
-    plt.hlines([0.5], ax.get_xlim()[0], ax.get_xlim()[1], linestyle=":", color="lightgray")
-    plt.ylim(1e-4, 1)
-    plt.xticks(np.linspace(570, 650, 5))
-    plt.yscale("log")
-    plt.xlabel("Read Current ($\mu$A)")
-    plt.ylabel("Bit Error Rate")
-    plt.grid(True)
-    plt.legend(frameon=False, bbox_to_anchor=(1, 1), loc="upper left")
-    plt.title("Write Current Sweep")
-    return ax
-
-
-def plot_enable_write_sweep_multiple(data_dict: dict):
-    fig, ax = plt.subplots()
-    for key in data_dict.keys():
-        plot_write_sweep_single(data_dict, key, ax)
-    return ax
 
 
 if __name__ == "__main__":
@@ -70,4 +27,7 @@ if __name__ == "__main__":
         "SPG806_20240920_nMem_parameter_sweep_D6_A4_C1_2024-09-20 17-45-14.mat"
     )
     data_dict = {0: data0, 1: data1, 2: data2, 3: data3, 4: data4, 5: data5}
-    plot_enable_write_sweep_multiple(data_dict)
+    fig, ax = plt.subplots()
+    plot_read_sweep_array(ax, data_dict, "bit_error_rate", "write_current")
+    ax.set_yscale("log")
+    ax.set_ylim(1e-4, 1)
