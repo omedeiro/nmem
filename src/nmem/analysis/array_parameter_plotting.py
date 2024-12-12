17b292693a5451e8445ae7c3b567810a2c5ba87b
omedeiro <omedeiro@mit.edu>
2024-12-12 14:02:25 -0500

    rename. fix xloc error that stoped text from plotting.

--- src/nmem/analysis/array_parameter_plotting.py
+++ src/nmem/analysis/array_parameter_plotting.py
@@ -12,7 +12,7 @@ from nmem.calculations.calculations import (
 from nmem.measurement.cells import CELLS
 
 # plt.rcParams["figure.figsize"] = [10, 12]
-plt.rcParams["font.size"] = 18
+plt.rcParams["font.size"] = 10
 
 
 def initialize_dict(array_size: tuple) -> dict:
@@ -35,7 +35,7 @@ def initialize_dict(array_size: tuple) -> dict:
         "enable_read_power": np.zeros(array_size),
     }
 
-def process_cell(cell: dict, param_dict: dict, x: int, y: int):
+def process_cell(cell: dict, param_dict: dict, x: int, y: int) -> dict:
     param_dict["write_current"][y, x] = cell["write_current"] * 1e6
     param_dict["read_current"][y, x] = cell["read_current"] * 1e6
     param_dict["enable_write_current"][y, x] = cell["enable_write_current"] * 1e6
@@ -67,12 +67,16 @@ def process_cell(cell: dict, param_dict: dict, x: int, y: int):
 
 
 if __name__ == "__main__":
-    xloc = []
-    yloc = []
+    xloc_list = []
+    yloc_list = []
     ARRAY_SIZE = (4, 4)
     param_dict = initialize_dict(ARRAY_SIZE)
     for c in CELLS:
-        process_cell(CELLS[c], param_dict, *convert_location_to_coordinates(c))
-
-    plot_array(xloc, yloc, param_dict["write_current"], "Write Current [$\mu$A]")
+        xloc, yloc = convert_location_to_coordinates(c)
+        param_dict = process_cell(CELLS[c], param_dict, xloc, yloc)
+        xloc_list.append(xloc)
+        yloc_list.append(yloc)
+    
+    fig, ax = plt.subplots()
+    plot_array(ax, xloc_list, yloc_list, param_dict["write_current"], "Write Current [$\mu$A]")
   
