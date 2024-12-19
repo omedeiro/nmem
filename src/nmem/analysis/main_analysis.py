59f147442245faed7aa9f988e7d64230744c6c05
omedeiro <omedeiro@mit.edu>
2024-12-19 15:43:33 -0500

    add titles. adjust spacing.

--- src/nmem/analysis/main_analysis.py
+++ src/nmem/analysis/main_analysis.py
@@ -87,7 +87,7 @@ if __name__ == "__main__":
         log=True,
         cmap=plt.get_cmap("Blues").reversed(),
     )
-
+    axs["bit_error"].set_title("Bit Error Rate")
     plot_array(
         axs["write"],
         xloc_list,
@@ -96,6 +96,7 @@ if __name__ == "__main__":
         log=False,
         cmap=plt.get_cmap("Reds"),
     )
+    axs["write"].set_title("Write Current (uA)")
     plot_array(
         axs["read"],
         xloc_list,
@@ -104,22 +105,26 @@ if __name__ == "__main__":
         log=False,
         cmap=plt.get_cmap("Blues"),
     )
+    axs["read"].set_title("Read Current (uA)")
     plot_array(
         axs["enable_write"],
         xloc_list,
         yloc_list,
-        param_dict["enable_write_current"],
+        param_dict["enable_write_power"]*1e6,
         log=False,
         cmap=plt.get_cmap("Reds"),
     )
+    axs["enable_write"].set_title("Enable Write Power (uW)")
     plot_array(
         axs["enable_read"],
         xloc_list,
         yloc_list,
-        param_dict["enable_read_current"],
+        param_dict["enable_read_power"]*1e6,
         log=False,
         cmap=plt.get_cmap("Blues"),
     )
+    axs["enable_read"].set_title("Enable Read Power (uW)")
 
+    fig.subplots_adjust(hspace=0.5, wspace=0.5)
     fig.patch.set_visible(False)
-    # plt.savefig("main_analysis.pdf", bbox_inches="tight")
+    plt.savefig("main_analysis.pdf", bbox_inches="tight")
