00081eb449ec1aefdf33e5113779a65670243ea7
omedeiro <omedeiro@mit.edu>
2025-01-13 16:10:38 -0500

    update import. remove main block debug code.

--- src/nmem/measurement/functions.py
+++ src/nmem/measurement/functions.py
@@ -24,7 +24,7 @@ from scipy.optimize import curve_fit
 from scipy.stats import norm
 from tqdm import tqdm
 
-from nmem.analysis.analysis import get_fitting_points, plot_fit
+from nmem.analysis.analysis import get_fitting_points, plot_fit, build_array
 from nmem.calculations.calculations import (
     calculate_critical_current,
     calculate_heater_power,
@@ -1460,43 +1460,3 @@ def write_sequence(
     load_waveforms(b, measurement_settings, chan=channel)
     write_waveforms(b, write_string, channel)
 
-
-if __name__ == "__main__":
-    import scipy.io as sio
-
-    data_dict = sio.loadmat(
-        r"S:\SC\Measurements\SPG806\D6\A4\20241220_nMem_measure_enable_response\C2\SPG806_20241220_nMem_measure_enable_response_D6_A4_C2_2024-12-20 13-44-29.mat"
-    )
-    data_dict2 = sio.loadmat(
-        r"S:\SC\Measurements\SPG806\D6\A4\20241220_nMem_measure_enable_response\C3\SPG806_20241220_nMem_measure_enable_response_D6_A4_C3_2024-12-20 17-28-57.mat"
-    )
-
-    fig, axs = plt.subplots()
-    x, y, ztotal = build_array(data_dict, "total_switches_norm")
-    xfit, yfit = get_fitting_points(x, y, ztotal)
-    axs.plot(xfit, yfit, label="C2", linestyle="-")
-    split_idx = 7
-    plot_fitting(axs, xfit[split_idx + 1 :], yfit[split_idx + 1 :])
-
-    split_idx = 10
-    x2, y2, ztotal2 = build_array(data_dict2, "total_switches_norm")
-
-    xfit, yfit = get_fitting_points(x2, y2, ztotal2)
-    axs.plot(xfit, yfit, label="C3", linestyle="-")
-    axs.legend()
-    axs.set_ylim([0, 1000])
-    axs.set_xlim([0, 500])
-
-    fig, axs = plt.subplots(1, 2, figsize=(10, 10))
-
-    plot_fitting(
-        axs[0], xfit[split_idx + 1 :], yfit[split_idx + 1 :], label="C3", linestyle="-"
-    )
-    axs[0].set_ylim([0, 1000])
-    axs[0].set_xlim([0, 500])
-    axs[0].plot(xfit, yfit, label="C2", linestyle="-")
-
-    plot_fitting(axs[1], xfit[:split_idx], yfit[:split_idx], label="C3", linestyle="-")
-    axs[1].plot(xfit, yfit, label="C2", linestyle="-")
-    axs[1].set_ylim([0, 1000])
-    axs[1].set_xlim([0, 500])
