fd53f263a02f181d6d93c36a3660874a986eec2d
omedeiro <omedeiro@mit.edu>
2025-01-30 18:17:53 -0500

    update plot

--- src/nmem/analysis/read_delay/read_delay_v2.py
+++ src/nmem/analysis/read_delay/read_delay_v2.py
@@ -1,5 +1,6 @@
 from nmem.analysis.analysis import import_directory, get_bit_error_rate
 import matplotlib.pyplot as plt
+import numpy as np
 
 if __name__ == "__main__":
     dict_list = import_directory("data2")
@@ -13,6 +14,12 @@ if __name__ == "__main__":
         bit_error_rate_list.append(bit_error_rate)
 
         print(f"delay: {delay}, bit_error_rate: {bit_error_rate}")
-
+    fidelity = 1 - np.array(bit_error_rate_list)
     fig, ax = plt.subplots()
-    ax.plot(delay_list, bit_error_rate_list, marker="o")
+    ax.plot(delay_list, fidelity, marker="o")
+    ax.plot(delay_list[10], fidelity[10], marker="d", color="red")
+    ax.plot(delay_list[9], fidelity[9], marker="s", color="red")
+    ax.set_xscale("log")
+
+    ax.set_xlabel("Memory Retention Time (s)")
+    ax.set_ylabel("Fidelity")
