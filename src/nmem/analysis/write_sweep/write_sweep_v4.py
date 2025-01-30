0764c9b6329c2bdd6f4bf44fd471fded1cd97790
omedeiro <omedeiro@mit.edu>
2025-01-30 14:47:45 -0500

    port data and script from read_margin

---
+++ src/nmem/analysis/write_sweep/write_sweep_v4.py
@@ -0,0 +1,16 @@
+import os
+
+import matplotlib.pyplot as plt
+import scipy.io as sio
+from nmem.analysis.analysis import import_directory, plot_read_sweep_array
+
+plt.rcParams["figure.figsize"] = [6, 4]
+plt.rcParams["font.size"] = 14
+
+
+if __name__ == "__main__":
+    fig, ax = plt.subplots()
+    dict_list = import_directory("data4")
+    plot_read_sweep_array(ax, dict_list, "bit_error_rate", "write_current")
+    ax.set_yscale("log")
+    ax.set_ylim(1e-4, 1)
