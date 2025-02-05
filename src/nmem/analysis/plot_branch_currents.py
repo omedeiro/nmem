1209ef6c216c3fc844994af64e3a2251f62436f6
omedeiro <omedeiro@mit.edu>
2025-02-05 17:46:26 -0500

    plot update

--- src/nmem/analysis/plot_branch_currents.py
+++ src/nmem/analysis/plot_branch_currents.py
@@ -1,9 +1,9 @@
-from nmem.analysis.analysis import plot_branch_currents
+from nmem.analysis.analysis import plot_branch_currents, plot_calculated_state_currents
 import matplotlib.pyplot as plt
 import numpy as np
 
 CRITICAL_TEMP = 12.3
-CRITICAL_CURRENT_ZERO = 1300
+CRITICAL_CURRENT_ZERO = 910
 ALPHA = 0.563
 RETRAP = 0.573
 WIDTH = 1 / 3
@@ -11,11 +11,14 @@ WIDTH = 1 / 3
 if __name__ == "__main__":
     fig, ax = plt.subplots()
     temps = np.linspace(0, CRITICAL_TEMP, 100)
-    plot_branch_currents(
-        ax, temps, CRITICAL_TEMP, RETRAP, WIDTH, CRITICAL_CURRENT_ZERO
+
+    plot_branch_currents(ax, temps, CRITICAL_TEMP, RETRAP, WIDTH, CRITICAL_CURRENT_ZERO*1.16)
+
+    ax.axhline(
+        CRITICAL_CURRENT_ZERO, color="black", linestyle="--", label="$I_{{C}}(0)$"
     )
     ax.set_xlabel("Temperature [K]")
-    ax.set_ylabel("Current [au]")
+    ax.set_ylabel("Current [$\mu$A]")
     ax.legend(frameon=False, loc="upper left", bbox_to_anchor=(1, 1))
     ax.grid()
     ax.set_xlim(0, 12.3)
