95b5258b49d68bb3e41b28daf00b85e30166e137
omedeiro <omedeiro@mit.edu>
2025-02-07 14:44:43 -0500

    create new plots

--- src/nmem/analysis/enable_write_current_sweep/enable_write_current_sweep.py
+++ src/nmem/analysis/enable_write_current_sweep/enable_write_current_sweep.py
@@ -8,45 +8,40 @@ from nmem.analysis.analysis import (
     plot_state_current_markers,
     get_state_current_markers_list,
     plot_waterfall,
+    get_state_current_markers,
+    get_write_current,
 )
-
-plt.rcParams["figure.figsize"] = [6, 4]
-plt.rcParams["font.size"] = 12
 
 
 if __name__ == "__main__":
     dict_list = import_directory("data")
 
     fig, ax = plt.subplots()
-    ax, ax2 = plot_enable_write_sweep_multiple(ax, dict_list[3:-1:2])
+    ax, ax2 = plot_enable_write_sweep_multiple(ax, dict_list[0:7])
     plt.savefig("enable_write_sweep.pdf", bbox_inches="tight")
     plt.show()
     ax2.xaxis.set_major_locator(MultipleLocator(0.1))
     
-    
+    colors = {
+        0: "red",
+        1: "red",
+        2: "blue",
+        3: "blue",
+    }
     fig, ax = plt.subplots()
-    for i in range(0, len(dict_list)):
-        plot_enable_write_sweep_single(ax, dict_list[i])
-        plot_state_current_markers(ax, dict_list[i], "enable_write_current")
-    ax.set_xlabel("Enable Write Current ($\mu$A)")
-    ax.set_ylabel("Write Current ($\mu$A)")
-    plt.show()
- 
-    fig, ax = plt.subplots()
-    state_current_marker_list = get_state_current_markers_list(dict_list, "enable_write_current")
-    for state_current_marker in state_current_marker_list:
-        ax.plot(state_current_marker[0], state_current_marker[1], marker="o")
-    
-    # fig, ax = plt.subplots()
-    # plot_operating_points(ax, dict_list, "write_current")
-    # plt.show()
+    for data_dict in dict_list:
+       state_current_markers = get_state_current_markers(data_dict, "enable_write_current")
+       write_current = get_write_current(data_dict)
+       for i, state_current in enumerate(state_current_markers[0, :]):
+           if state_current > 0:
+               ax.plot(
+                   write_current,
+                   state_current,
+                   "o",
+                   label=f"{write_current} $\mu$A",
+                   markerfacecolor=colors[i],
+                   markeredgecolor="none",
+               )
 
-    # fig, ax = plt.subplots()
-    # plot_operating_margins(ax, dict_list, "write_current")
-    # plt.show()
-
-    # fig, ax = plt.subplots(subplot_kw={"projection": "3d"}, figsize=(16, 9))
-    # plot_waterfall(ax, dict_list[::2])
-    # ax.view_init(45, 90)
-
-    # plt.show()
+    ax.set_xlabel("$I_{\mathrm{write}}$ [$\mu$A]")
+    ax.set_ylabel("$I_{\mathrm{enable}}$ [$\mu$A]")
--- src/nmem/analysis/write_current_sweep_operation.py
+++ src/nmem/analysis/write_current_sweep_operation.py
@@ -1,30 +1,77 @@
+
+
+
+
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator
 import numpy as np
 from nmem.analysis.analysis import (
     import_directory,
-    plot_channel_temperature,
+    plot_enable_write_sweep_multiple,
+    plot_enable_write_sweep_single,
+    plot_state_current_markers,
+    get_state_current_markers_list,
+    plot_waterfall,
+    get_state_current_markers,
+    get_write_current,
     plot_write_sweep,
-    get_read_current,
     get_bit_error_rate,
     get_bit_error_rate_args,
     get_read_currents,
     get_enable_write_current,
     get_write_temperature,
+    get_read_current,
+    get_state_current_markers,
+    get_write_current,
 )
-
-SUBSTRATE_TEMP = 1.3
-CRITICAL_TEMP = 12.3
-
 
 
 if __name__ == "__main__":
+    dict_list = import_directory(r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\enable_write_current_sweep\data")
+
+    fig, axs = plt.subplot_mosaic("AB;CD", figsize=(8.3, 4), width_ratios=[1, 0.25], constrained_layout=True)
+
+    ax = axs["A"]
+    ax, ax2 = plot_enable_write_sweep_multiple(ax, dict_list[0:7])
+    ax.set_ylabel("BER")
+    ax2.set_xlabel("$T_{\mathrm{write}}$ [K]")
+    ax.set_xlabel("$I_{\mathrm{enable}}$ [$\mu$A]")
+    
+
+
+    colors = {
+        0: "red",
+        1: "red",
+        2: "blue",
+        3: "blue",
+    }
+    ax = axs["B"]
+    for data_dict in dict_list:
+       state_current_markers = get_state_current_markers(data_dict, "enable_write_current")
+       write_current = get_write_current(data_dict)
+       for i, state_current in enumerate(state_current_markers[0, :]):
+           if state_current > 0:
+               ax.plot(
+                   write_current,
+                   state_current,
+                   "o",
+                   label=f"{write_current} $\mu$A",
+                   markerfacecolor=colors[i],
+                   markeredgecolor="none",
+               )
+
+    ax.set_xlabel("$I_{\mathrm{write}}$ [$\mu$A]")
+    ax.set_ylabel("$I_{\mathrm{enable}}$ [$\mu$A]")
+
+
+
+
+    ax = axs["C"]
     dict_list = import_directory(
         r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\write_current_sweep_enable_write\data"
     )
     dict_list = dict_list[1:]
-    fig, axs = plt.subplots(1, 2, figsize=(6, 4), width_ratios=[1, 0.25])
     dict_list = dict_list[::-1]
-    ax = axs[0]
     plot_write_sweep(ax, dict_list)
     ax.set_xlabel("$I_{\mathrm{write}}$ [$\mu$A]")
     ax.set_ylabel("BER")
@@ -65,11 +112,11 @@ if __name__ == "__main__":
     ax.axvline(110, color="grey", linestyle="--")
     ax.axvline(140, color="grey", linestyle="--")
 
-    ax = axs[1]
+    ax = axs["D"]
 
 
-    ax.plot(ichl_temp, ichl_current_list, marker="o")
-    ax.plot(ichr_temp, ichr_current_list, marker="o")
+    ax.plot(ichl_temp, ichl_current_list, marker="o", color="blue")
+    ax.plot(ichr_temp, ichr_current_list, marker="o", color="red")
     ax.set_xlabel("$T_{\mathrm{write}}$ [K]")
     ax.set_ylabel("$I_{\mathrm{write}}$ [$\mu$A]")
     ax.set_ylim(0,300)
@@ -77,3 +124,4 @@ if __name__ == "__main__":
 
 
     fig.subplots_adjust(wspace=0.3)
+    fig.savefig("write_current_sweep_operation.pdf", bbox_inches="tight")
