ca7a9175e5b393214c303299625c436c2cbd5158
omedeiro <omedeiro@mit.edu>
2025-02-14 13:37:05 -0500

    add to list

--- src/nmem/analysis/array_parameter_plotting/main_analysis_table.py
+++ src/nmem/analysis/array_parameter_plotting/main_analysis_table.py
@@ -11,6 +11,10 @@ if __name__ == "__main__":
     critical_current_zero_list = []
     critical_current_intercept_list = []
     max_heater_current_list = []
+    enable_read_current_list = []
+    enable_write_current_list = []
+    resistance_cryo_list = []
+    fig, ax = plt.subplots()
     for c in CELLS:
         cell_list.append(c)
         max_critical_current = CELLS[c]["max_critical_current"] * 1e6
@@ -24,6 +28,13 @@ if __name__ == "__main__":
         critical_current_intercept_list.append(critical_current_intercept)
         max_heater_current = CELLS[c]["x_intercept"]
         max_heater_current_list.append(max_heater_current)
+        enable_read_current = CELLS[c]["enable_read_current"] * 1e6
+        enable_read_current_list.append(enable_read_current)
+        enable_write_current = CELLS[c]["enable_write_current"] * 1e6
+        enable_write_current_list.append(enable_write_current)
+        resistance_cryo = CELLS[c]["resistance_cryo"]
+        resistance_cryo_list.append(resistance_cryo)
+
     df = pd.DataFrame(
         {
             "Cell": cell_list,
@@ -31,6 +42,9 @@ if __name__ == "__main__":
             "Zero": critical_current_zero_list,
             "Y-Intercept": critical_current_intercept_list,
             "Max Heater Current": max_heater_current_list,
+            "Enable Read Current": enable_read_current_list,
+            "Enable Write Current": enable_write_current_list,
+            "Resistance Cryo": resistance_cryo_list,
         },
         columns=[
             "Cell",
@@ -38,7 +52,10 @@ if __name__ == "__main__":
             "Zero",
             "Y-Intercept",
             "Max Heater Current",
+            "Enable Read Current",
+            "Enable Write Current",
+            "Resistance Cryo",
         ],
     )
-
+    ax.plot(df["Cell"], df["Y-Intercept"], "o")
     print(df)
