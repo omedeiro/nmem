b618a40ca7f071b267eb07d3e460a5cd52bb8937
omedeiro <omedeiro@mit.edu>
2025-02-19 15:56:00 -0500

    param plot update

--- src/nmem/analysis/plot_persistent_current.py
+++ src/nmem/analysis/plot_persistent_current.py
@@ -6,15 +6,15 @@ from nmem.analysis.analysis import (
 )
 import matplotlib.pyplot as plt
 import numpy as np
-
+from nmem.measurement.functions import calculate_power
 
 SUBSTRATE_TEMP = 1.3
 CRITICAL_TEMP = 12.3
 
 
-ALPHA = 0.563
-RETRAP = 0.573
-WIDTH = 1 / 2.13
+ALPHA = 0.4
+RETRAP = 0.7
+WIDTH = 0.33
 
 if __name__ == "__main__":
 
@@ -22,11 +22,13 @@ if __name__ == "__main__":
         r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\enable_write_current_sweep\data"
     )
     data_dict = dict_list[0]
+    power = calculate_power(data_dict)
     persistent_current = 30
     fig, ax = plt.subplots()
     critical_current_zero = get_critical_current_intercept(data_dict)*0.88
 
     temperatures = np.linspace(0, CRITICAL_TEMP, 100)
+    
     plot_calculated_state_currents(
         ax,
         temperatures,
@@ -53,7 +55,7 @@ if __name__ == "__main__":
     ax.set_ylabel("Current [au]")
     ax.grid()
     ax.set_xlim(0, CRITICAL_TEMP)
-
+    ax.plot([7], [800], marker="x", color="black", markersize=10)
     ax.legend(frameon=False, loc="upper left", bbox_to_anchor=(1, 1))
 
     # ax.set_ylim(0, 1)
