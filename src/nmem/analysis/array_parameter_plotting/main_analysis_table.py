54687204ec5da4d014be451df9eb1f569a1e81fd
omedeiro <omedeiro@mit.edu>
2025-02-21 13:07:17 -0500

    update table

--- src/nmem/analysis/array_parameter_plotting/main_analysis_table.py
+++ src/nmem/analysis/array_parameter_plotting/main_analysis_table.py
@@ -2,60 +2,71 @@ import matplotlib.pyplot as plt
 
 
 from nmem.measurement.cells import CELLS
-from nmem.analysis.analysis import calculate_critical_current_zero
+from nmem.analysis.analysis import (
+    calculate_critical_current_zero,
+    calculate_channel_temperature,
+    CRITICAL_TEMP,
+    SUBSTRATE_TEMP,
+)
 import pandas as pd
 
+ALPHA_FIT = 0.88
+BETA_FIT = 1.25
+WIDTH_LEFT = 100e-9
+WIDTH_RIGHT = 300e-9
+THICKNESS = 23e-9
 if __name__ == "__main__":
-    cell_list = []
-    critical_current_list = []
-    critical_current_zero_list = []
-    critical_current_intercept_list = []
-    max_heater_current_list = []
-    enable_read_current_list = []
-    enable_write_current_list = []
-    resistance_cryo_list = []
+
     fig, ax = plt.subplots()
-    for c in CELLS:
-        cell_list.append(c)
-        max_critical_current = CELLS[c]["max_critical_current"] * 1e6
-        critical_current_list.append(max_critical_current)
+    data = []
+    cell_dictionary = CELLS
+    for c in cell_dictionary:
+        critical_current_tsub = cell_dictionary[c]["max_critical_current"] * 1e6
 
-        critical_current_zero = calculate_critical_current_zero(
-            12.3, 1.3, max_critical_current
+        critical_current_tzero = calculate_critical_current_zero(
+            CRITICAL_TEMP, SUBSTRATE_TEMP, critical_current_tsub
         )
-        critical_current_zero_list.append(critical_current_zero)
-        critical_current_intercept = CELLS[c]["y_intercept"]
-        critical_current_intercept_list.append(critical_current_intercept)
-        max_heater_current = CELLS[c]["x_intercept"]
-        max_heater_current_list.append(max_heater_current)
-        enable_read_current = CELLS[c]["enable_read_current"] * 1e6
-        enable_read_current_list.append(enable_read_current)
-        enable_write_current = CELLS[c]["enable_write_current"] * 1e6
-        enable_write_current_list.append(enable_write_current)
-        resistance_cryo = CELLS[c]["resistance_cryo"]
-        resistance_cryo_list.append(resistance_cryo)
+        critical_current_intercept = cell_dictionary[c]["y_intercept"]
+        critical_current_tzero_alpha = critical_current_intercept * ALPHA_FIT
 
-    df = pd.DataFrame(
-        {
-            "Cell": cell_list,
-            "Critical Current": critical_current_list,
-            "Zero": critical_current_zero_list,
-            "Y-Intercept": critical_current_intercept_list,
-            "Max Heater Current": max_heater_current_list,
-            "Enable Read Current": enable_read_current_list,
-            "Enable Write Current": enable_write_current_list,
-            "Resistance Cryo": resistance_cryo_list,
-        },
-        columns=[
-            "Cell",
-            "Critical Current",
-            "Zero",
-            "Y-Intercept",
-            "Max Heater Current",
-            "Enable Read Current",
-            "Enable Write Current",
-            "Resistance Cryo",
-        ],
-    )
-    ax.plot(df["Cell"], df["Y-Intercept"], "o")
-    print(df)
+        max_heater_current = cell_dictionary[c]["x_intercept"]
+        enable_read_current = cell_dictionary[c]["enable_read_current"] * 1e6
+        enable_write_current = cell_dictionary[c]["enable_write_current"] * 1e6
+        resistance_cryo = cell_dictionary[c]["resistance_cryo"]
+
+        read_temperature = calculate_channel_temperature(
+            CRITICAL_TEMP, SUBSTRATE_TEMP, enable_read_current, max_heater_current
+        ).flatten()[0]
+        write_temperature = calculate_channel_temperature(
+            CRITICAL_TEMP, SUBSTRATE_TEMP, enable_write_current, max_heater_current
+        ).flatten()[0]
+
+        critical_current_density_zero = (
+            critical_current_tsub
+            * 1e-6
+            / (WIDTH_LEFT * THICKNESS + WIDTH_RIGHT * THICKNESS)
+        ) * (1 - (SUBSTRATE_TEMP / CRITICAL_TEMP) ** 3) ** -2.1
+
+
+        data.append(
+            {
+                "Cell": c,
+                "Critical Current Tsub": critical_current_tsub,
+                # "Critical Current Tzero": critical_current_tzero,
+                "Y-Intercept": critical_current_intercept,
+                # "Critical Current Tzero Alpha": critical_current_tzero_alpha,
+                "Max Heater Current": max_heater_current,
+                "Read Temperature": read_temperature,
+                "Write Temperature": write_temperature,
+                # "Enable Read Current": enable_read_current,
+                # "Enable Write Current": enable_write_current,
+                "Resistance Cryo": resistance_cryo,
+                "Critical Current Density Zero [MA/cm2]": critical_current_density_zero
+                * 1e-10,
+            }
+        )
+
+    df = pd.DataFrame(data)
+    df.style.format("{:.3f}")
+
+    df.to_csv("array_parameter_table.csv", float_format="%.3f")
