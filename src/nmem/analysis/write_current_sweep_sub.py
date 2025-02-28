5bc8713ec96a31622186702818af84385564553b
omedeiro <omedeiro@mit.edu>
2025-02-28 15:09:37 -0500

    update

--- src/nmem/analysis/write_current_sweep_sub.py
+++ src/nmem/analysis/write_current_sweep_sub.py
@@ -20,24 +20,34 @@ from nmem.analysis.analysis import (
     CRITICAL_TEMP,
 )
 
+ALPHA = 0.53
 WIDTH = 0.35
-RETRAP = 0.2
+RETRAP = 0.26
 IWRITE_XLIM = 100
 IWRITE_XLIM_2 = 300
-MAX_PERSISTENT=50
+MAX_PERSISTENT = 50
 RBCOLORS = {0: "black", 1: "black", 2: "grey", 3: "grey"}
-if __name__ == "__main__":
-    dict_list = import_directory(
-        r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\enable_write_current_sweep\data"
+
+
+def get_branch_currents(data_dict):
+    read_temperature = np.array([get_channel_temperature(data_dict, "read")])
+    critical_current_zero = get_critical_current_intercept(data_dict)
+    channel_current_zero = calculate_critical_current_temp(
+        read_temperature,
+        CRITICAL_TEMP,
+        critical_current_zero,
     )
+    ichl, irhl, ichr, irhr = calculate_branch_currents(
+        read_temperature, CRITICAL_TEMP, RETRAP, WIDTH, channel_current_zero
+    )
+    return ichl, irhl, ichr, irhr
 
-    fig, ax = plt.subplots()
 
-    persistent_current = np.linspace(0, 100, 1000)
-    persistent_current = np.where(persistent_current > MAX_PERSISTENT, MAX_PERSISTENT, persistent_current)
+def process_dict_list(dict_list):
     write_temp_array = np.empty((len(dict_list), 4))
     write_current_array = np.empty((len(dict_list), 1))
     write_channel_array = np.empty((len(dict_list), 4))
+
     for j, data_dict in enumerate(dict_list):
         if j == 0:
             read_temperature = np.array([get_channel_temperature(data_dict, "read")])
@@ -47,30 +57,53 @@ if __name__ == "__main__":
                 CRITICAL_TEMP,
                 critical_current_zero,
             )
-            ichl, irhl, ichr, irhr = calculate_branch_currents(
-                read_temperature, CRITICAL_TEMP, RETRAP, WIDTH, channel_current_zero
-            )
-            print(f"ichl: {ichl},\n irhl: {irhl},\n ichr: {ichr},\n irhr: {irhr}")
 
         bit_error_rate = get_bit_error_rate(data_dict)
         berargs = get_bit_error_rate_args(bit_error_rate)
         write_current = get_write_current(data_dict)
-        write_critical_currents = calculate_critical_current_temp(
-            np.array([get_channel_temperature(data_dict, "write")]),
-            CRITICAL_TEMP,
-            critical_current_zero,
-        )
+        read_current = get_read_current(data_dict)
+
         write_temps = get_channel_temperature_sweep(data_dict)
+        write_critical_currents = calculate_critical_current_temp(
+            write_temps,
+            CRITICAL_TEMP,
+            get_critical_current_heater_off(data_dict),
+        )
         write_current_array[j] = write_current
-        critical_current_zero = get_critical_current_heater_off(data_dict)
         for i, arg in enumerate(berargs):
             if arg is not np.nan:
                 write_temp_array[j, i] = write_temps[arg]
                 write_channel_array[j, i] = calculate_critical_current_temp(
                     write_temps[arg],
                     CRITICAL_TEMP,
-                    critical_current_zero,
+                    get_critical_current_heater_off(data_dict),
                 )
+    return (
+        write_current_array,
+        write_temp_array,
+        write_channel_array,
+        write_critical_currents,
+    )
+
+
+if __name__ == "__main__":
+    dict_list = import_directory(
+        r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\enable_write_current_sweep\data"
+    )
+    ichl, irhl, ichr, irhr = get_branch_currents(dict_list[0])
+
+    fig, ax = plt.subplots()
+
+    persistent_current = np.linspace(0, 100, 1000)
+    persistent_current = np.where(persistent_current > irhl, irhl, persistent_current)
+    read_current = get_read_current(dict_list[0])
+    (
+        write_current_array,
+        write_temp_array,
+        write_channel_array,
+        write_critical_currents,
+    ) = process_dict_list(dict_list)
+
     for i in range(4):
         ax.plot(
             write_current_array,
@@ -81,7 +114,7 @@ if __name__ == "__main__":
         )
     limits = ax.get_ylim()
     ic_limits = calculate_critical_current_temp(
-        np.array(limits), CRITICAL_TEMP, critical_current_zero
+        np.array(limits), CRITICAL_TEMP, get_critical_current_intercept(dict_list[0])
     )
 
     ax.set_ylabel("$I_{\mathrm{CH}}$ [$\mu$A]")
@@ -90,9 +123,26 @@ if __name__ == "__main__":
     ax.grid(axis="x")
     ax.set_xlabel("$I_{\mathrm{write}}$ [$\mu$A]")
 
-    ax.axhline(ichl + irhr, color="grey", linestyle="--")
-    ax.axhline(ichr, color="red", linestyle="--")
-    ax.axhline(ichr + irhl, color="black", linestyle="--")
+    ax.axhline(ichl + irhr, color="grey", linestyle="--", label="ichl + irhr")
+    ax.axhline(ichr, color="red", linestyle="--", label="ichr")
+    ax.axhline(ichr + irhl, color="black", linestyle="--", label="ichr + irhl")
+    ax.axhline(
+        read_current * ALPHA, color="green", linestyle="--", label="i_L"
+    )
+    ax.axhline(
+        read_current * (1 - ALPHA),
+        color="green",
+        linestyle="--",
+        label="i_R",
+    )
 
-    # ax2 = ax.twinx()
-    # ax2.plot(np.linspace(0, IWRITE_XLIM, 1000), persistent_current, color="black", linestyle="-")
+    ax.legend()
+    ax2 = ax.twinx()
+    ax2.plot(
+        np.linspace(0, IWRITE_XLIM, 1000),
+        persistent_current,
+        color="black",
+        linestyle="-",
+    )
+
+    ax.axvline(irhl, color="black", linestyle="--")
