3972dcb32633b71064d5cffceb37d338274d856f
omedeiro <omedeiro@mit.edu>
2025-03-03 13:13:53 -0500

    delete rcparams. combined plot

--- src/nmem/analysis/dc_sweep/dc_sweep.py
+++ src/nmem/analysis/dc_sweep/dc_sweep.py
@@ -8,27 +8,6 @@ from nmem.analysis.analysis import (
     plot_current_voltage_from_dc_sweep,
 )
 
-font_path = r"C:\\Users\\ICE\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Inter-VariableFont_opsz,wght.ttf"
-fm.fontManager.addfont(font_path)
-prop = fm.FontProperties(fname=font_path)
-plt.rcParams["figure.figsize"] = [3.5, 3.5]
-plt.rcParams["font.size"] = 5
-plt.rcParams["axes.linewidth"] = 0.5
-plt.rcParams["xtick.major.width"] = 0.5
-plt.rcParams["ytick.major.width"] = 0.5
-plt.rcParams["xtick.direction"] = "in"
-plt.rcParams["ytick.direction"] = "in"
-plt.rcParams["font.family"] = "Inter"
-plt.rcParams["lines.markersize"] = 2
-plt.rcParams["lines.linewidth"] = 0.5
-plt.rcParams["legend.fontsize"] = 5
-plt.rcParams["legend.frameon"] = False
-
-
-plt.rcParams["xtick.major.size"] = 1
-plt.rcParams["ytick.major.size"] = 1
-
-
 def plot_combined_figure(ax: Axes, dict_list: list, save: bool = False) -> Axes:
     ax[0, 0].axis("off")
     ax[0, 1].axis("off")
@@ -53,8 +32,4 @@ if __name__ == "__main__":
 
     fig, ax = plt.subplots()
     plot_current_voltage_from_dc_sweep(ax, data_list)
-    plt.show()
-
-    fig, ax = plt.subplots(2, 3, figsize=(7, 3.5), height_ratios=[1, 0.7])
-    plot_combined_figure(ax, data_list)
     plt.show()
