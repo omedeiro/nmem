ccf19a2ae135df67333d2c98abe1ae34fc823c16
Owen Medeiros <omedeiro@mit.edu>
2025-03-10 17:49:58 -0400

    refactor nmem_cell_plot.py to accept file path as an argument, add max output calculation, and implement plot_all_write_sweeps function; update I_write step parameters in simulation files

--- src/nmem/simulation/spice_circuits/nmem_cell_plot.py
+++ src/nmem/simulation/spice_circuits/nmem_cell_plot.py
@@ -48,14 +48,14 @@ def get_ichr(ltspice_data: ltspice.Ltspice, case: int = 0) -> np.ndarray:
     return np.array([ltspice_data.get_data("V(ichr)", case=case)[0]*1e6])
 
 
-def plot_nmem_cell():
-    file_path = "spice_simulation_raw/nmem_cell_write_step_5uA.raw"
+def plot_nmem_cell(file_path: str):
     l = ltspice.Ltspice(file_path)
     l.parse()  # Parse the raw file
     colors = [CMAP(i) for i in np.linspace(0, 1, l.case_count)]
     fig, ax = plt.subplots()
     persistent_currents = np.zeros(l.case_count)
     write_currents = np.zeros(l.case_count)
+    max_outputs = np.zeros(l.case_count)
     for i in range(0, l.case_count):
         ax = plot_data(ax, l, "Ix(HL:drain)", case=i, color=colors[i])
         ax = plot_data(ax, l, "Ix(HR:drain)", case=i, color=colors[i], linestyle="--")
@@ -64,7 +64,8 @@ def plot_nmem_cell():
 
         persistent_currents[i] = get_persistent_current(l, case=i)
         write_currents[i] = get_write_current(l, case=i)
-    
+        max_outputs[i] = get_max_output(l, case=i)
+
     ax.set_xlabel("Time (s)")
     ax.set_ylabel("Voltage (V)")
     fig.savefig("nmem_cell_write.png")
@@ -73,11 +74,12 @@ def plot_nmem_cell():
     fig, ax = plt.subplots()
     ax.plot(write_currents, persistent_currents, "-o")
     
-    
-    ax.axhline(get_irhl(l), color="k", linestyle="--", label="IRHL")
-    ax.axhline(get_irhr(l), color="k", linestyle="-.", label="IRHR")
-    ax.axhline(get_ichl(l), color="k", linestyle=":", label="ICHL")
-    ax.axvline(get_ichr(l), color="k", linestyle="-", label="ICHR")
+    ax2 = ax.twinx()
+    ax2.plot(write_currents, max_outputs*1e3, "-o", color="r", label="Voltage Output")  
+    ax2.set_ylabel("Peak Voltage Output (mV)")
+
+
+
     ax.plot([0, ax.get_ylim()[1]*2], [0, ax.get_ylim()[1]], "--", label="y=x/2")
 
     ax.legend()
@@ -87,6 +89,17 @@ def plot_nmem_cell():
     fig.savefig(f"nmem_cell_write_slope.png")
     plt.show()
 
+def get_max_output(ltspice_data: ltspice.Ltspice, case: int = 0) -> np.ndarray:
+    signal = ltspice_data.get_data("V(out)", case=case)
+    return np.max(signal)
+
     
+def plot_all_write_sweeps():
+    plot_nmem_cell("spice_simulation_raw/nmem_cell_write_200uA.raw")
+    plot_nmem_cell("spice_simulation_raw/nmem_cell_write_1000uA.raw")
+    plot_nmem_cell("spice_simulation_raw/nmem_cell_write_step_5uA.raw")
+    plot_nmem_cell("spice_simulation_raw/nmem_cell_write_step_10uA.raw")
+
+
 if __name__ == "__main__":
-    plot_nmem_cell()
+    plot_nmem_cell("nmem_cell_write.raw")
