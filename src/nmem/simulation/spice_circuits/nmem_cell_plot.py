fb825840cbeda947468d78c89c0091cb576f121b
Owen Medeiros <omedeiro@mit.edu>
2025-03-10 16:10:31 -0400

    enhance nmem_cell_plot.py with additional plotting functions and case handling for LTspice data. write current v persistent current plot

--- src/nmem/simulation/spice_circuits/nmem_cell_plot.py
+++ src/nmem/simulation/spice_circuits/nmem_cell_plot.py
@@ -1,36 +1,65 @@
 import ltspice
 import numpy as np
 import matplotlib.pyplot as plt
-
+CMAP = plt.get_cmap("coolwarm")
 
 def plot_data(
-    ax: plt.Axes, ltspice_data: ltspice.Ltspice, signal_name: str
+    ax: plt.Axes, ltspice_data: ltspice.Ltspice, signal_name: str, case: int = 0, **kwargs
 ) -> plt.Axes:
-    time = ltspice_data.get_time()
-    signal = ltspice_data.get_data(signal_name)
-    ax.plot(time, signal, label=signal_name)
+    time = ltspice_data.get_time(case=case)
+    signal = ltspice_data.get_data(signal_name, case=case)
+    ax.plot(time, signal, label=signal_name, **kwargs)
     return ax
 
 
-# Path to your LTspice .raw file
-file_path = "spice_simulation_raw/nmem_cell_write_read_clear.raw"
+def plot_write_read_clear():
+    file_path = "spice_simulation_raw/nmem_cell_write_read_clear.raw"
+    l = ltspice.Ltspice(file_path)
+    l.parse()  # Parse the raw file
 
-# Load the LTspice data
-l = ltspice.Ltspice(file_path)
-l.parse()  # Parse the raw file
+    fig, ax = plt.subplots()
+    ax = plot_data(ax, l, "Ix(HR:drain)")
+    ax = plot_data(ax, l, "V(ichr)")
 
-# Extract specific signals
-time = l.get_time()  # Time vector
-voltage = l.get_data("Ix(HR:drain)")  # Example: Node voltage
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Voltage (V)")
+    ax.legend()
+    plt.show()
 
-# Plot the extracted data
-fig, ax = plt.subplots()
-ax = plot_data(ax, l, "Ix(HR:drain)")
-# ax = plot_data(ax, l, "Ix(HL:drain)")
-# ax = plot_data(ax, l, "V(ichl)")
-ax = plot_data(ax, l, "V(ichr)")
+def get_persistent_current(ltspice_data: ltspice.Ltspice, case: int = 0) -> np.ndarray:
+    signal_l = ltspice_data.get_data("Ix(HL:drain)", case=case)
+    signal_r = ltspice_data.get_data("Ix(HR:drain)", case=case)
+    print(f"signal_l: {signal_l[-1]*1e6}, signal_r: {signal_r[-1]*1e6}")
+    return np.array([signal_r[-1]*1e6])
 
-ax.set_xlabel("Time (s)")
-ax.set_ylabel("Voltage (V)")
-ax.legend()
-plt.show()
+def get_write_current(ltspice_data: ltspice.Ltspice, case: int = 0) -> np.ndarray:
+    signal = ltspice_data.get_data("I(R2)", case=case)
+    return np.max(signal)*1e6
+
+def plot_nmem_cell():
+    file_path = "nmem_cell_write.raw"
+    l = ltspice.Ltspice(file_path)
+    l.parse()  # Parse the raw file
+    colors = [CMAP(i) for i in np.linspace(0, 1, l.case_count)]
+    fig, ax = plt.subplots()
+    persistent_currents = np.zeros(l.case_count)
+    write_currents = np.zeros(l.case_count)
+    for i in range(0, l.case_count):
+        ax = plot_data(ax, l, "Ix(HL:drain)", case=i, color=colors[i])
+        ax = plot_data(ax, l, "Ix(HR:drain)", case=i, color=colors[i], linestyle="--")
+        persistent_currents[i] = get_persistent_current(l, case=i)
+        write_currents[i] = get_write_current(l, case=i)
+
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Voltage (V)")
+
+    fig, ax = plt.subplots()
+    ax.plot(write_currents, persistent_currents, "-o")
+
+    ax.plot([0, 300], [0, 150], "r--", label="y = x/2")
+    ax.legend()
+    ax.set_xlabel("Write Current (uA)")
+    ax.set_ylabel("Persistent Current (uA)")
+
+if __name__ == "__main__":
+    plot_nmem_cell()
