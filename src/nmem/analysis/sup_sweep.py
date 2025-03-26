e3770ef860c4a827b2c4ac3a3c74f49fc8e68567
Owen Medeiros <omedeiro@mit.edu>
2025-03-26 14:19:47 -0400

    add new script for plotting write current sweep and enabling sweep analysis

---
+++ src/nmem/analysis/sup_sweep.py
@@ -0,0 +1,39 @@
+from nmem.analysis.analysis import import_directory, plot_read_sweep_array
+from nmem.analysis.write_current_sweep_operation_v2 import plot_enable_sweep
+import matplotlib.pyplot as plt
+import os
+
+
+def plot_write_current_sweep(
+    ax: plt.Axes, dict_list: list[dict[str, list[float]]]
+) -> plt.Axes:
+    plot_read_sweep_array(ax, dict_list, "bit_error_rate", "write_current")
+    ax.set_xlabel("Read Current [$\mu$A]")
+    ax.set_ylabel("Bit Error Rate")
+    # ax.legend(
+    #     frameon=False, bbox_to_anchor=(1.1, 1), loc="upper left", title="Write Current"
+    # )
+    
+    return ax
+
+def main():
+    fig, axs = plt.subplot_mosaic("A;B", figsize=(90/25.4, 150/25.4))
+    write_current_sweep = import_directory(
+        "/home/omedeiro/nmem/src/nmem/analysis/read_current_sweep_write_current2/write_current_sweep_C3",
+    )
+    plot_write_current_sweep(axs["A"], write_current_sweep)
+
+    # ax = axs["A"]
+    dict_list = import_directory(
+        os.path.join(os.path.dirname(__file__), "enable_write_current_sweep/data")
+    )
+    sort_dict_list = sorted(
+        dict_list, key=lambda x: x.get("write_current").flatten()[0]
+    )
+    plot_enable_sweep(axs["B"], sort_dict_list, range=slice(0, len(sort_dict_list)))
+
+    plt.savefig("sup_full_param_sweeps.pdf", bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    main()
