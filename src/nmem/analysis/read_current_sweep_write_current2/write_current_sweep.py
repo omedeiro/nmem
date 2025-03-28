007be1d89c3411ca4bebbced43c8114de9b9b89f
Owen Medeiros <omedeiro@mit.edu>
2025-03-27 21:15:06 -0400

    create plot for sup mat

--- src/nmem/analysis/read_current_sweep_write_current2/write_current_sweep.py
+++ src/nmem/analysis/read_current_sweep_write_current2/write_current_sweep.py
@@ -3,44 +3,31 @@ import matplotlib.pyplot as plt
 from nmem.analysis.analysis import (
     import_directory,
     plot_read_sweep_array,
+    get_channel_temperature
 )
 
 
 def plot_read_temp_sweep_C3(save=True):
     fig, axs = plt.subplots(2, 2, figsize=(12, 6))
-
-    plot_read_sweep_array(
-        axs[0, 0],
-        import_directory("write_current_sweep_C3_2"),
-        "bit_error_rate",
-        "write_current",
-    )
-    axs[0, 0].set_xlabel("Read Current [$\mu$A]")
-    axs[0, 0].set_ylabel("Bit Error Rate")
-    plot_read_sweep_array(
-        axs[0, 1],
+    dict_list = [
+        import_directory("write_current_sweep_C3_1"),
         import_directory("write_current_sweep_C3_3"),
-        "bit_error_rate",
-        "write_current",
-    )
-    axs[0, 1].set_xlabel("Read Current [$\mu$A]")
-    axs[0, 1].set_ylabel("Bit Error Rate")
-    plot_read_sweep_array(
-        axs[1, 0],
         import_directory("write_current_sweep_C3_4"),
-        "bit_error_rate",
-        "write_current",
-    )
-    axs[1, 0].set_xlabel("Read Current [$\mu$A]")
-    axs[1, 0].set_ylabel("Bit Error Rate")
-    plot_read_sweep_array(
-        axs[1, 1],
         import_directory("write_current_sweep_C3"),
-        "bit_error_rate",
-        "write_current",
-    )
-    axs[1, 1].set_xlabel("Read Current [$\mu$A]")
-    axs[1, 1].set_ylabel("Bit Error Rate")
+    ]
+    for i, data_dict in enumerate(dict_list):
+        enable_temperature = get_channel_temperature(data_dict[0], "read")
+        plot_read_sweep_array(
+            axs[i // 2, i % 2],
+            data_dict,
+            "bit_error_rate",
+            "write_current",
+            marker=".",
+        )
+        axs[i // 2, i % 2].set_xlabel("Read Current [$\mu$A]")
+        axs[i // 2, i % 2].set_ylabel("Bit Error Rate")
+        axs[i // 2, i % 2].set_title(f"Read Temperature: {enable_temperature:0.2f} $\mu$A")
+   
     axs[0, 1].legend(
         frameon=False,
         bbox_to_anchor=(1.1, 1),
@@ -55,6 +42,8 @@ def plot_read_temp_sweep_C3(save=True):
 def plot_read_sweep_import(data_dict: dict[str, list[float]]):
     fig, ax = plt.subplots()
     plot_read_sweep_array(ax, data_dict, "bit_error_rate", "write_current")
+    cell = data_dict[0]["cell"][0]
+
     ax.set_xlabel("Read Current [$\mu$A]")
     ax.set_ylabel("Bit Error Rate")
     ax.legend(
@@ -63,15 +52,16 @@ def plot_read_sweep_import(data_dict: dict[str, list[float]]):
         bbox_to_anchor=(1, 1),
         title="Write Current [$\mu$A]",
     )
+    ax.set_title(f"Cell {cell}")
     return fig, ax
 
 if __name__ == "__main__":
-    plot_read_sweep_import(import_directory("write_current_sweep_B2_0"))
-    plot_read_sweep_import(import_directory("write_current_sweep_B2_1"))
-    plot_read_sweep_import(import_directory("write_current_sweep_B2_2"))
+    # plot_read_sweep_import(import_directory("write_current_sweep_B2_0"))
+    # plot_read_sweep_import(import_directory("write_current_sweep_B2_1"))
+    # plot_read_sweep_import(import_directory("write_current_sweep_B2_2"))
 
-    plot_read_sweep_import(import_directory("write_current_sweep_A2"))
-    plot_read_sweep_import(import_directory("write_current_sweep_C2"))
+    # plot_read_sweep_import(import_directory("write_current_sweep_A2"))
+    # plot_read_sweep_import(import_directory("write_current_sweep_C2"))
 
 
     plot_read_temp_sweep_C3()
