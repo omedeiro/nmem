1a9906c6228161713d2505063210b6112307d398
Owen Medeiros <omedeiro@mit.edu>
2025-03-31 14:17:06 -0400

    refactor plotting functions to include dedicated colorbar support and improve layout; add new plot_read_temp_sweep_C3_v2 function for enhanced data visualization. sup figures

--- src/nmem/analysis/analysis.py
+++ src/nmem/analysis/analysis.py
@@ -1103,7 +1103,7 @@ def plot_linear_fit(ax: Axes, xfit: np.ndarray, yfit: np.ndarray, add_text:bool=
     x_intercept = -z[1] / z[0]
     # ax.scatter(xfit, yfit, color="#08519C")
     xplot = np.linspace(0, x_intercept, 10)
-    ax.plot(xplot, p(xplot), "--", color="#740F15")
+    ax.plot(xplot, p(xplot), ":", color="k")
     if add_text:
         ax.text(
             0.1,
@@ -1336,7 +1336,7 @@ def plot_read_sweep_switch_probability(
 
 
 def plot_fill_between_array(ax: Axes, dict_list: list[dict]) -> Axes:
-    colors = CMAP(np.linspace(0.1, 1, len(dict_list)))
+    colors = CMAP3(np.linspace(0.1, 1, len(dict_list)))
     for i, data_dict in enumerate(dict_list):
         plot_fill_between(ax, data_dict, colors[i])
     return ax
@@ -1351,7 +1351,7 @@ def plot_read_sweep_array(
     add_errorbar=False,
     **kwargs,
 ) -> Axes:
-    colors = CMAP(np.linspace(0, 1, len(dict_list)))
+    colors = CMAP3(np.linspace(0, 1, len(dict_list)))
     variable_list = []
     for i, data_dict in enumerate(dict_list):
         plot_read_sweep(
@@ -1808,8 +1808,26 @@ def plot_grid(axs: Axes, dict_list: list[dict]) -> Axes:
             label=f"{cell}",
             color=colors[column],
             marker=markers[row],
+        )
+        dd = sio.loadmat(
+            "/home/omedeiro/nmem/src/nmem/analysis/enable_current_relation_v2/data2/SPG806_20250107_nMem_measure_enable_response_D6_A4_A3_2025-01-07 15-34-05.mat"
+        )
+        y_step_size = y[1] - y[0]
+        print(x)
+        print(y)
+        axs[row, column].errorbar(
+            xfit,
+            yfit,
+            yerr=y_step_size* np.ones_like(yfit),
+            fmt="o",
+            color=colors[column],
+            marker=markers[row],
             markeredgecolor="k",
             markeredgewidth=0.1,
+            markersize=5,
+            alpha=0.5,
+            zorder=1,
+            label="_data",
         )
 
         xfit, yfit = filter_plateau(xfit, yfit, yfit[0] * 0.75)
@@ -1951,67 +1969,78 @@ ALPHA = 0.25
 RETRAP = 1
 WIDTH = 0.33
 
-if __name__ == "__main__":
-    from nmem.measurement.functions import calculate_power
+# if __name__ == "__main__":
+#     from nmem.measurement.functions import calculate_power
 
+#     dict_list = import_directory(
+#         r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\enable_write_current_sweep\data"
+#     )
+#     data_dict = dict_list[0]
+#     power = calculate_power(data_dict)
+#     persistent_current = 80
+#     fig, ax = plt.subplots()
+#     # critical_current_zero = get_critical_current_intercept(data_dict)*0.88
+#     critical_current_zero = 1250
+#     temperatures = np.linspace(0, CRITICAL_TEMP, 100)
+
+#     plot_calculated_state_currents(
+#         ax,
+#         temperatures,
+#         CRITICAL_TEMP,
+#         RETRAP,
+#         WIDTH,
+#         ALPHA,
+#         persistent_current,
+#         critical_current_zero,
+#     )
+
+#     plot_calculated_filled_region(
+#         ax,
+#         temperatures,
+#         data_dict,
+#         persistent_current,
+#         CRITICAL_TEMP,
+#         RETRAP,
+#         WIDTH,
+#         ALPHA,
+#         critical_current_zero,
+#     )
+#     ax.set_xlabel("Temperature [K]")
+#     ax.set_ylabel("Current [au]")
+#     ax.grid()
+#     ax.set_xlim(0, CRITICAL_TEMP)
+#     ax.plot([7], [800], marker="x", color="black", markersize=10)
+#     ax.legend(frameon=False, loc="upper left", bbox_to_anchor=(1, 1))
+
+#     data_dict1 = sio.loadmat("measured_state_currents_290.mat")
+#     data_dict2 = sio.loadmat("measured_state_currents_300.mat")
+#     data_dict3 = sio.loadmat("measured_state_currents_310.mat")
+
+#     dict_list = [data_dict1, data_dict2, data_dict3]
+#     fit_results = []
+#     for data_dict in [dict_list[2]]:
+#         temp = data_dict["measured_temperature"].flatten()
+#         state_currents = data_dict["measured_state_currents"]
+#         x_list = []
+#         y_list = []
+#         for i in range(4):
+#             x = temp
+#             y = state_currents[:, i]
+#             x, y = filter_nan(x, y)
+#             ax.plot(x, y, "-o", color=RBCOLORS[i], label=f"State {i}")
+
+#         ax.set_xlim(6, 9)
+#         ax.set_ylim(500, 900)
+#     # ax.set_ylim(0, 1)
+#     plt.show()
+
+
+if __name__ == "__main__": 
     dict_list = import_directory(
-        r"C:\Users\ICE\Documents\GitHub\nmem\src\nmem\analysis\enable_write_current_sweep\data"
-    )
-    data_dict = dict_list[0]
-    power = calculate_power(data_dict)
-    persistent_current = 80
-    fig, ax = plt.subplots()
-    # critical_current_zero = get_critical_current_intercept(data_dict)*0.88
-    critical_current_zero = 1250
-    temperatures = np.linspace(0, CRITICAL_TEMP, 100)
-
-    plot_calculated_state_currents(
-        ax,
-        temperatures,
-        CRITICAL_TEMP,
-        RETRAP,
-        WIDTH,
-        ALPHA,
-        persistent_current,
-        critical_current_zero,
+    "/home/omedeiro/nmem/src/nmem/analysis/enable_current_relation_v2/data"
     )
 
-    plot_calculated_filled_region(
-        ax,
-        temperatures,
-        data_dict,
-        persistent_current,
-        CRITICAL_TEMP,
-        RETRAP,
-        WIDTH,
-        ALPHA,
-        critical_current_zero,
-    )
-    ax.set_xlabel("Temperature [K]")
-    ax.set_ylabel("Current [au]")
-    ax.grid()
-    ax.set_xlim(0, CRITICAL_TEMP)
-    ax.plot([7], [800], marker="x", color="black", markersize=10)
-    ax.legend(frameon=False, loc="upper left", bbox_to_anchor=(1, 1))
-
-    data_dict1 = sio.loadmat("measured_state_currents_290.mat")
-    data_dict2 = sio.loadmat("measured_state_currents_300.mat")
-    data_dict3 = sio.loadmat("measured_state_currents_310.mat")
-
-    dict_list = [data_dict1, data_dict2, data_dict3]
-    fit_results = []
-    for data_dict in [dict_list[2]]:
-        temp = data_dict["measured_temperature"].flatten()
-        state_currents = data_dict["measured_state_currents"]
-        x_list = []
-        y_list = []
-        for i in range(4):
-            x = temp
-            y = state_currents[:, i]
-            x, y = filter_nan(x, y)
-            ax.plot(x, y, "-o", color=RBCOLORS[i], label=f"State {i}")
-
-        ax.set_xlim(6, 9)
-        ax.set_ylim(500, 900)
-    # ax.set_ylim(0, 1)
+    fig, axs = plt.subplots(5, 5, figsize=(180/25.4, 180/25.4), sharex=True, sharey=True)
+    plot_full_grid(axs, dict_list)
+    # plt.savefig("enable_current_relation_full_grid.pdf", bbox_inches="tight")
     plt.show()
