7241f25f64df338b4a89b4e29a8a335a5533b81b
Owen Medeiros <omedeiro@mit.edu>
2025-04-15 14:03:55 -0400

    Refactor plot_geom_loop_size.py: streamline plotting code and enhance subplot organization for better readability

--- src/nmem/analysis/geom_loop_size/plot_geom_loop_size.py
+++ src/nmem/analysis/geom_loop_size/plot_geom_loop_size.py
@@ -3,40 +3,61 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 from matplotlib.cm import get_cmap
+from nmem.analysis.analysis import set_plot_style
+from matplotlib.ticker import LogFormatter
 
+set_plot_style()
 # Load all .mat files in the current directory
 loop_sizes = np.arange(1.7, 5.2, 0.5)
+
+
 def import_mat_list(path):
     files = sorted([f for f in os.listdir(path) if f.endswith(".mat")])
-    data = [sio.loadmat(os.path.join(path, f), squeeze_me=True, struct_as_record=False) for f in files]
-    return [d['data'] if 'data' in d else d for d in data]
+    data = [
+        sio.loadmat(os.path.join(path, f), squeeze_me=True, struct_as_record=False)
+        for f in files
+    ]
+    return [d["data"] if "data" in d else d for d in data]
+
 
 # Load data
 data = import_mat_list(os.getcwd())
 N = len(data)
-NMEAS=1000
+NMEAS = 1000
 # Define colors (similar to 'MIndexed10', using color cycle here)
-cmap = get_cmap("tab10")
+# cmap = get_cmap("tab10")
 
 # First plot: Vch vs. ber_est
-plt.figure(1)
+fig, axs = plt.subplots(1, 3, figsize=(7, 2.5), sharey=True)
+ax = axs[0]
 for i in range(N):
-    Vch = np.ravel(data[i]["Vch"])
+    Vch = np.ravel(data[i]["Vch"]) * 1e3
     ber_est = np.ravel(data[i]["ber_est"])
     err = np.sqrt(ber_est * (1 - ber_est) / NMEAS)
-    plt.errorbar(Vch, ber_est, yerr=err, fmt='o', color=cmap(i), label=f"_Loop Size {loop_sizes[i]:.1f} au")
-    plt.plot(Vch, ber_est, color=cmap(i), label=f"Loop Size {loop_sizes[i]:.1f} $\mu$A")
-plt.yscale("log")
-plt.xlabel("channel voltage (mV)")
-plt.ylabel("estimated BER")
-plt.legend(loc="lower left")
+    # plt.errorbar(Vch, ber_est, yerr=err, fmt='o', color=cmap(i), label=f"_Loop Size {loop_sizes[i]:.1f} au")
+    ax.plot(Vch, ber_est, label=f"$w_{{5}}$ = {loop_sizes[i]:.1f} $\mu$A")
+ax.set_yscale("log")
+ax.set_xlabel("channel voltage (mV)")
+ax.set_ylabel("estimated BER")
+ax.legend(loc="lower left")
 
 # Second plot: best BER vs loop size
 best_ber = [np.min(np.ravel(d["ber_est"])) for d in data]
-plt.figure(2)
-plt.plot(range(1, N + 1), best_ber, '-o')
-plt.yscale("log")
-plt.xlabel("loop size (au)")
-plt.ylabel("best BER")
 
+ax = axs[1]
+ax.plot(range(1, N + 1), best_ber, "-o")
+ax.set_yscale("log")
+ax.set_xlabel("loop size ($\mu$m)")
+ax.set_ylabel("minimum BER")
+ax.yaxis.set_major_locator(plt.LogLocator(base=10.0, numticks=10))
+ax.xaxis.set_major_locator(plt.MaxNLocator(integer=True))
+fig.subplots_adjust(left=0.1, right=0.95, top=0.9, bottom=0.1, wspace=0.2)
+
+axs[2].axis("off")
+plt.savefig(
+    "geom_loop_size.pdf",
+    dpi=300,
+    bbox_inches="tight",
+    pad_inches=0.1,
+)
 plt.show()
