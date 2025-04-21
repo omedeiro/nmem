a156d297f0348f1f01282eb657cb69e1563d6cf2
Owen Medeiros <omedeiro@mit.edu>
2025-04-21 13:00:56 -0400

    Refactor voltage_trace_emulate_slow: implement trace extraction and plotting functions for concatenated voltage traces

--- src/nmem/analysis/voltage_trace_emulate_slow/voltage_trace_emulate_slow.py
+++ src/nmem/analysis/voltage_trace_emulate_slow/voltage_trace_emulate_slow.py
@@ -1,18 +1,69 @@
 import matplotlib.pyplot as plt
-
+from matplotlib.axes import Axes
+from matplotlib.ticker import MultipleLocator
+import numpy as np
+from typing import List, Tuple
 from nmem.analysis.analysis import (
     import_directory,
-    plot_voltage_trace_stack,
+    CMAP,
+    get_voltage_trace_data,
+    plot_voltage_trace,
 )
 
 plt.rcParams["figure.figsize"] = [6, 4]
 plt.rcParams["font.size"] = 10
 
 
+def extract_shifted_traces(
+    data_dict: dict, trace_index: int = 0, time_shift: float = 0.0
+) -> Tuple:
+    chan_in_x, chan_in_y = get_voltage_trace_data(
+        data_dict, "trace_chan_in", trace_index
+    )
+    chan_out_x, chan_out_y = get_voltage_trace_data(
+        data_dict, "trace_chan_out", trace_index
+    )
+    enab_in_x, enab_in_y = get_voltage_trace_data(data_dict, "trace_enab", trace_index)
+
+    # Shift all x values
+    chan_in_x = chan_in_x + time_shift
+    chan_out_x = chan_out_x + time_shift
+    enab_in_x = enab_in_x + time_shift
+
+    return chan_in_x, chan_in_y, enab_in_x, enab_in_y, chan_out_x, chan_out_y
+
+
+def plot_time_concatenated_traces(axs: List[Axes], dict_list: List[dict]) -> List[Axes]:
+    colors = CMAP(np.linspace(0.1, 1, len(dict_list)))
+    colors = np.flipud(colors)
+
+    for idx, data_dict in enumerate(dict_list):
+        shift = 10 * idx  # Shift time window by 10 µs per dataset
+        chan_in_x, chan_in_y, enab_in_x, enab_in_y, chan_out_x, chan_out_y = (
+            extract_shifted_traces(data_dict, time_shift=shift)
+        )
+
+        plot_voltage_trace(axs[0], chan_in_x, chan_in_y, color=colors[0])
+        plot_voltage_trace(axs[1], enab_in_x, enab_in_y, color=colors[1])
+        plot_voltage_trace(axs[2], chan_out_x, chan_out_y, color=colors[-1])
+
+    axs[2].xaxis.set_major_locator(MultipleLocator(10))
+    axs[2].xaxis.set_major_formatter(plt.FuncFormatter(lambda x, _: f"{int(x)}"))
+
+    axs[2].set_xlim(0, 50)
+
+    fig = plt.gcf()
+    fig.supylabel("Voltage [mV]")
+    fig.supxlabel("Time [$\mu$s]")
+    fig.subplots_adjust(hspace=0.0)
+
+    return axs
+
+
 if __name__ == "__main__":
     dict_list = import_directory("data")
 
-    for data_dict in dict_list:
-        fig, axs = plt.subplots(3, 1)
-        plot_voltage_trace_stack(axs, data_dict)
-        plt.show()
+    fig, axs = plt.subplots(3, 1, figsize=(7, 3), sharex=True)
+    plot_time_concatenated_traces(axs, dict_list[:5])
+    plt.savefig("voltage_trace_emulate_slow.pdf", bbox_inches="tight")
+    plt.show()
