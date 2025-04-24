b4c0fce419fa51c92afe199444462eaebb0c04ca
Owen Medeiros <omedeiro@mit.edu>
2025-04-24 14:47:33 -0400

    fix: update axis label formatting for consistency in read current sweep plot

--- src/nmem/analysis/read_current_sweep_enable_read/read_current_sweep_three.py
+++ src/nmem/analysis/read_current_sweep_enable_read/read_current_sweep_three.py
@@ -15,28 +15,6 @@ from nmem.analysis.read_current_sweep_write_current2.write_current_sweep import 
 )
 
 set_plot_style()
-# font_path = r"C:\\Users\\ICE\\AppData\\Local\\Microsoft\\Windows\\Fonts\\Inter-VariableFont_opsz,wght.ttf"
-# fm.fontManager.addfont(font_path)
-# prop = fm.FontProperties(fname=font_path)
-
-# plt.rcParams.update(
-#     {
-#         "figure.figsize": [3.5, 3.5],
-#         "font.size": 6,
-#         "axes.linewidth": 0.5,
-#         "xtick.major.width": 0.5,
-#         "ytick.major.width": 0.5,
-#         "xtick.direction": "out",
-#         "ytick.direction": "out",
-#         "font.family": "Inter",
-#         "lines.markersize": 2,
-#         "lines.linewidth": 1.2,
-#         "legend.fontsize": 5,
-#         "legend.frameon": False,
-#         "xtick.major.size": 1,
-#         "ytick.major.size": 1,
-#     }
-# )
 
 if __name__ == "__main__":
     data = import_directory("data")
@@ -62,9 +40,9 @@ if __name__ == "__main__":
         plot_fill_between_array(axs[i], dict_list[i])
         axs[i].set_xlim(400, 1000)
         axs[i].set_ylim(0, 1)
-        axs[i].set_xlabel("Read Current ($\mu$A)")
+        axs[i].set_xlabel("Read Current [µA]")
         axs[i].set_title(
-            f"$I_{{EW}}$ = {290 + i * 10} [$\mu$A]\n$T_{{W}}$ = {enable_write_temp:.2f} [K]"
+            f"$I_{{EW}}$ = {290 + i * 10} [µA]\n$T_{{W}}$ = {enable_write_temp:.2f} [K]"
         )
         axs[i].set_box_aspect(1.0)
         axs[i].xaxis.set_major_locator(plt.MultipleLocator(200))
