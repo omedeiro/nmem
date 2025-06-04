382d06305c14c2368eb27b2713adccaab1cfe2a9
Owen Medeiros <omedeiro@mit.edu>
2025-06-04 12:53:06 -0400

    rename analysis to core_analysis.py

--- src/nmem/analysis/core_analysis.py
+++ src/nmem/analysis/core_analysis.py
@@ -16,79 +16,6 @@ SUBSTRATE_TEMP = 1.3
 CRITICAL_TEMP = 12.3
 
 
-def import_write_sweep_formatted() -> list[dict]:
-    dict_list = import_directory(
-        os.path.join(os.path.dirname(__file__), "write_current_sweep_enable_write/data")
-    )
-    dict_list = dict_list[1:]
-    dict_list = dict_list[::-1]
-    dict_list = sorted(
-        dict_list, key=lambda x: x.get("enable_write_current").flatten()[0]
-    )
-    return dict_list
-
-
-def import_delay_dict() -> dict:
-    dict_list = import_directory(
-        os.path.join(os.path.dirname(__file__), "read_delay_v2/data3")
-    )
-    delay_list = []
-    bit_error_rate_list = []
-    for data_dict in dict_list:
-        delay = data_dict.get("delay").flatten()[0] * 1e-3
-        bit_error_rate = get_bit_error_rate(data_dict)
-
-        delay_list.append(delay)
-        bit_error_rate_list.append(bit_error_rate)
-
-    delay_dict = {}
-    delay_dict["delay"] = delay_list
-    delay_dict["bit_error_rate"] = bit_error_rate_list
-    return delay_dict
-
-
-def import_write_sweep_formatted_markers(dict_list) -> list[dict]:
-    data = []
-    data2 = []
-    for data_dict in dict_list:
-        bit_error_rate = get_bit_error_rate(data_dict)
-        berargs = get_bit_error_rate_args(bit_error_rate)
-        write_currents = get_read_currents(
-            data_dict
-        )  # This is correct. "y" is the write current in this .mat.
-        enable_write_current = get_enable_write_current(data_dict)
-        read_current = get_read_current(data_dict)
-        write_current = get_write_current(data_dict)
-
-        for i, arg in enumerate(berargs):
-            if arg is not np.nan:
-
-                if i == 0:
-                    data.append(
-                        {
-                            "write_current": write_currents[arg],
-                            "write_temp": get_channel_temperature(data_dict, "write"),
-                            "read_current": read_current,
-                            "enable_write_current": enable_write_current,
-                        }
-                    )
-                if i == 2:
-                    data2.append(
-                        {
-                            "write_current": write_currents[arg],
-                            "write_temp": get_channel_temperature(data_dict, "write"),
-                            "read_current": read_current,
-                            "enable_write_current": enable_write_current,
-                        }
-                    )
-    data_dict = {
-        "data": data,
-        "data2": data2,
-    }
-    return data_dict
-
-
-
 def build_array(
     data_dict: dict, parameter_z: Literal["total_switches_norm"]
 ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
@@ -330,11 +257,6 @@ def get_average_response(cell_dict):
     return slope, intercept
 
 
-def get_file_names(file_path: str) -> list:
-    files = os.listdir(file_path)
-    files = [file for file in files if file.endswith(".mat")]
-    return files
-
 
 def get_text_from_bit(bit: str) -> str:
     if bit == "0":
@@ -549,14 +471,7 @@ def get_voltage_trace_data(
     return x, y
 
 
-def save_directory_list(file_path: str, file_list: list[str]) -> None:
-    with open(os.path.join(file_path, "data.txt"), "w") as f:
-        for file_name in file_list:
-            f.write(file_name + "\n")
 
-    f.close()
-
-    return
 
 
 def get_state_currents_measured(
@@ -618,16 +533,6 @@ def get_state_currents_array(dict_list: list[dict]) -> np.ndarray:
     return np.array(state_currents)
 
 
-def import_directory(file_path: str) -> list[dict]:
-    dict_list = []
-    files = get_file_names(file_path)
-    files = sorted(files)
-    for file in files:
-        data = sio.loadmat(os.path.join(file_path, file))
-        dict_list.append(data)
-
-    save_directory_list(file_path, files)
-    return dict_list
 
 
 def initialize_dict(array_size: tuple) -> dict:
