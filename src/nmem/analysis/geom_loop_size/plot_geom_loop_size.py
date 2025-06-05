6ff8bf07e0504d8ed7910ba849b97624546dce1d
Owen Medeiros <omedeiro@mit.edu>
2025-06-05 11:38:55 -0400

    refactor: streamline data loading and remove unused import functions

--- src/nmem/analysis/geom_loop_size/plot_geom_loop_size.py
+++ src/nmem/analysis/geom_loop_size/plot_geom_loop_size.py
@@ -5,28 +5,17 @@ import numpy as np
 import scipy.io as sio
 from matplotlib.ticker import LogLocator, MaxNLocator
 
-from nmem.analysis.analysis import set_plot_style
-
+from nmem.analysis.plotting import set_plot_style
+from nmem.analysis.data_import import import_directory
 set_plot_style()
-# Load all .mat files in the current directory
+
 loop_sizes = np.arange(1.7, 5.2, 0.5)
 
 
-def import_mat_list(path):
-    files = sorted([f for f in os.listdir(path) if f.endswith(".mat")])
-    data = [
-        sio.loadmat(os.path.join(path, f), squeeze_me=True, struct_as_record=False)
-        for f in files
-    ]
-    return [d["data"] if "data" in d else d for d in data]
-
-
 # Load data
-data = import_mat_list(os.getcwd())
+data = import_directory("data")
 N = len(data)
 NMEAS = 1000
-# Define colors (similar to 'MIndexed10', using color cycle here)
-# cmap = get_cmap("tab10")
 
 # First plot: Vch vs. ber_est
 fig, axs = plt.subplots(1, 3, figsize=(7, 2.5), sharey=True)
@@ -35,7 +24,6 @@ for i in range(N):
     Vch = np.ravel(data[i]["Vch"]) * 1e3
     ber_est = np.ravel(data[i]["ber_est"])
     err = np.sqrt(ber_est * (1 - ber_est) / NMEAS)
-    # plt.errorbar(Vch, ber_est, yerr=err, fmt='o', color=cmap(i), label=f"_Loop Size {loop_sizes[i]:.1f} au")
     ax.plot(Vch, ber_est, label=f"$w_{{5}}$ = {loop_sizes[i]:.1f} µm")
 ax.set_yscale("log")
 ax.set_xlabel("channel voltage [mV]")
@@ -55,10 +43,12 @@ ax.xaxis.set_major_locator(MaxNLocator(integer=True))
 fig.subplots_adjust(left=0.1, right=0.95, top=0.9, bottom=0.1, wspace=0.2)
 
 axs[2].axis("off")
-plt.savefig(
-    "geom_loop_size.pdf",
-    dpi=300,
-    bbox_inches="tight",
-    pad_inches=0.1,
-)
+save_fig = False
+if save_fig:
+    plt.savefig(
+        "geom_loop_size.pdf",
+        dpi=300,
+        bbox_inches="tight",
+        pad_inches=0.1,
+    )
 plt.show()
