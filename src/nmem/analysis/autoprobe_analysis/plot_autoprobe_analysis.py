f47d1d480fb85c734814ace687c9af769104e8ad
Owen Medeiros <omedeiro@mit.edu>
2025-06-06 15:16:57 -0400

    refactor: consolidate autoprobe analysis functionality by moving to a new script and removing redundant stats module

--- src/nmem/analysis/autoprobe_analysis/plot_autoprobe_analysis.py
+++ src/nmem/analysis/autoprobe_analysis/plot_autoprobe_analysis.py
@@ -1,12 +1,17 @@
-
 from nmem.analysis.data_import import load_autoprobe_data
 from nmem.analysis.plotting import plot_combined_histogram_and_die_maps, set_plot_style
+from nmem.analysis.utils import summarize_die_yield
 
 set_plot_style()
 
-if __name__ == "__main__":
+
+def main():
     df = load_autoprobe_data("autoprobe_parsed.mat")
     wafer_rows = ["1", "4", "6", "7"]
+    summary_df, row_stats_df = summarize_die_yield(
+        df, wafer_rows, min_kohm=0.1, max_kohm=500000
+    )
+
     limit_dict = {
         "1": [20, 100],
         "4": [20, 100],
@@ -14,3 +19,10 @@ if __name__ == "__main__":
         "7": [20, 100],
     }
     plot_combined_histogram_and_die_maps(df, wafer_rows, limit_dict)
+
+    print(summary_df)  # Die-level stats
+    print(row_stats_df.head())  # Row-level summary
+
+
+if __name__ == "__main__":
+    main()
