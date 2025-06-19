1cb7b99be7ccc29db2482198c6031b7d9936342b
Owen Medeiros <omedeiro@mit.edu>
2025-06-19 11:29:21 -0400

    refactor: update main function to use process_array_parameter_data for improved data handling

--- src/nmem/scripts/plot_array_parameter_matrix.py
+++ src/nmem/scripts/plot_array_parameter_matrix.py
@@ -1,38 +1,29 @@
 import matplotlib.pyplot as plt
 
 from nmem.analysis.core_analysis import (
-    initialize_dict,
-    process_cell,
+    process_array_parameter_data,  # newly refactored function
 )
 from nmem.analysis.matrix_plots import plot_parameter_array
-from nmem.analysis.utils import convert_cell_to_coordinates
 from nmem.measurement.cells import CELLS
 
 
-def main(array_size=(4, 4)):
+def main(save_dir=None):
     """
     Process cell data and plot parameter arrays for the given array size.
     """
-    xloc_list = []
-    yloc_list = []
-    param_dict = initialize_dict(array_size)
-    yintercept_list = []
-    slope_list = []
-    for c in CELLS:
-        xloc, yloc = convert_cell_to_coordinates(c)
-        param_dict = process_cell(CELLS[c], param_dict, xloc, yloc)
-        xloc_list.append(xloc)
-        yloc_list.append(yloc)
-        yintercept = CELLS[c]["y_intercept"]
-        yintercept_list.append(yintercept)
-        slope = CELLS[c]["slope"]
-        slope_list.append(slope)
+    xloc_list, yloc_list, param_dict, yintercept_list, slope_list = (
+        process_array_parameter_data(CELLS)
+    )
     fig, ax = plt.subplots()
     plot_parameter_array(
-        ax, xloc_list, yloc_list, param_dict["write_current"], "Write Current [$\mu$A]"
+        xloc_list,
+        yloc_list,
+        param_dict["write_current"],
+        "Write Current [$\\mu$A]",
+        ax=ax,
+        save_path=f"{save_dir}/write_current_array.png" if save_dir else None,
     )
-    plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main(save_dir="../plots")
