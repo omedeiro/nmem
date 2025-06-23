90b6edbcd11b9789aa850b68287e47de2840c063
Owen Medeiros <omedeiro@mit.edu>
2025-06-23 10:30:24 -0400

    refactor: improve code formatting and remove hardcoded save directory in main function

--- src/nmem/scripts/plot_array_fidelity_bar.py
+++ src/nmem/scripts/plot_array_fidelity_bar.py
@@ -18,6 +18,7 @@ set_inter_font()
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
+
 def generate_plots(ber_array, save_dir=None):
     """
     Generate the plots and save them to the specified directory.
@@ -25,11 +26,14 @@ def generate_plots(ber_array, save_dir=None):
     if save_dir:
         # Save the plots with a path provided
         plot_ber_3d_bar(ber_array, save_path=f"{save_dir}/ber_3d_bar.png")
-        plot_fidelity_clean_bar(ber_array, save_path=f"{save_dir}/fidelity_clean_bar.png")
+        plot_fidelity_clean_bar(
+            ber_array, save_path=f"{save_dir}/fidelity_clean_bar.png"
+        )
     else:
         # Display the plots if no save path is provided
         plot_ber_3d_bar(ber_array)
         plot_fidelity_clean_bar(ber_array)
+
 
 def main(save_dir=None):
     """
@@ -38,6 +42,7 @@ def main(save_dir=None):
     ber_array = process_ber_data(logger=logger)
     generate_plots(ber_array, save_dir)
 
+
 if __name__ == "__main__":
     # Call the main function
-    main(save_dir="../plots")
+    main()
