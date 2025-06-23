093b1c41f6912ab26447a99e71969cd720f64288
Owen Medeiros <omedeiro@mit.edu>
2025-06-23 14:21:53 -0400

    refactor: remove unused plot_ber_array import and streamline retention plotting

--- src/nmem/scripts/plot_ber_memory_retention.py
+++ src/nmem/scripts/plot_ber_memory_retention.py
@@ -1,14 +1,12 @@
 import matplotlib.pyplot as plt
 
 from nmem.analysis.data_import import import_delay_data
-from nmem.analysis.matrix_plots import plot_ber_array
 from nmem.analysis.sweep_plots import plot_retention
 
 
 def main(save_dir=None):
     delay_list, bit_error_rate_list, _ = import_delay_data()
-    fig, axs = plot_retention(delay_list, bit_error_rate_list)
-    plot_ber_array(axs["B"])
+    plot_retention(delay_list, bit_error_rate_list)
 
     if save_dir:
         plt.savefig(
