fix: update compute_sigma_separation to return sigma average and log results in plot_voltage_pulse_avg

src/nmem/analysis/core_analysis.py
@@ -394,7 +394,7 @@ def prepare_state_current_data(data_dict):
     return x_list, y_list
 
 
-def compute_sigma_separation(data: dict, show_print=True) -> float:
+def compute_sigma_separation(data: dict) -> float:
     """Compute the peak separation between read0 and read1 histograms in units of σ."""
     v_read0 = np.array(data["read_zero_top"])
     v_read1 = np.array(data["read_one_top"])
@@ -411,12 +411,8 @@ def compute_sigma_separation(data: dict, show_print=True) -> float:
     sigma_avg = 0.5 * (sigma0 + sigma1)
     separation_sigma = mu0 + sigma0 * 3 - (mu1 - 3 * sigma1)
 
-    if show_print:
-        print(f"μ0 = {mu0:.3f} mV, σ0 = {sigma0:.3f} mV")
-        print(f"μ1 = {mu1:.3f} mV, σ1 = {sigma1:.3f} mV")
-        print(f"Separation = {separation_sigma:.2f} σ")
 
-    return separation_sigma
+    return separation_sigma, sigma_avg
 
 
 def extract_shifted_traces(