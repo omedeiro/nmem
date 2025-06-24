c6e6da43d4e6ecd86f8c3e39f3d585040bf81840
Owen Medeiros <omedeiro@mit.edu>
2025-06-24 10:33:38 -0400

    fix: refactor main function to remove save_fig and output_path parameters, and update saving logic

--- src/nmem/scripts/plot_ber_enable_write_sweep_write_current_margin.py
+++ src/nmem/scripts/plot_ber_enable_write_sweep_write_current_margin.py
@@ -4,9 +4,7 @@ from nmem.analysis.data_import import import_directory
 from nmem.analysis.sweep_plots import plot_write_current_enable_sweep_margin
 
 
-def main(
-    data_dir="../data/ber_sweep_enable_write_current/data1", save_fig=False, output_path="write_current_enable_sweep_margin.pdf"
-):
+def main(data_dir="../data/ber_sweep_enable_write_current/data1", save_dir=None):
     """
     Main function to plot write current enable sweep margin.
     """
@@ -15,10 +13,19 @@ def main(
     ]
     dict_list = import_directory(data_dir)
     plot_write_current_enable_sweep_margin(
-        dict_list, inner, save_fig=save_fig, output_path=output_path
+        dict_list,
+        inner,
     )
-    plt.show()
 
+    if save_dir:
+        plt.savefig(
+            f"{save_dir}/ber_write_current_sweep_enable_margin.png",
+            dpi=300,
+            bbox_inches="tight",
+        )
+        plt.close()
+    else:
+        plt.show()
 
 if __name__ == "__main__":
     main()
