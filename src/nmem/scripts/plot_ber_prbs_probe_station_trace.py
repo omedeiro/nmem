78da6ab9af7b757fd83dc35d921cd5a8d24fa4b0
Owen Medeiros <omedeiro@mit.edu>
2025-06-24 11:30:32 -0400

    fix: refactor main function to replace save_fig and output_path parameters with save_dir for improved clarity

--- src/nmem/scripts/plot_ber_prbs_probe_station_trace.py
+++ src/nmem/scripts/plot_ber_prbs_probe_station_trace.py
@@ -1,4 +1,4 @@
-
+from matplotlib import pyplot as plt
 from nmem.analysis.core_analysis import analyze_prbs_errors
 from nmem.analysis.data_import import import_directory
 from nmem.analysis.trace_plots import plot_probe_station_prbs
@@ -11,9 +11,8 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
-def main(
-    data_dir="../data/ber_prbs_probe_station", trim=4500, save_fig=False, output_path="probe_station_prbs.pdf"
-):
+
+def main(data_dir="../data/ber_prbs_probe_station", trim=4500, save_dir=None):
     data_list = import_directory(data_dir)
     total_error, W1R0_error, W0R1_error, error_locs = analyze_prbs_errors(
         data_list, trim=trim
@@ -22,10 +21,18 @@ def main(
         data_list,
         trim=trim,
         error_locs=error_locs,
-        save_fig=save_fig,
-        output_path=output_path,
     )
     logger.info(f"Total errors: {total_error}, W1R0: {W1R0_error}, W0R1: {W0R1_error}")
+
+    if save_dir:
+        fig.savefig(
+            f"{save_dir}/ber_prbs_probe_station_trace.png",
+            dpi=300,
+            bbox_inches="tight",
+        )
+        plt.close(fig)
+    else:
+        plt.show()
 
 
 if __name__ == "__main__":
