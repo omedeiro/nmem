4b90d4d94d4274c59974354c1a63987cdbca5eec
Owen Medeiros <omedeiro@mit.edu>
2025-07-02 13:22:42 -0400

    refactor: enhance legend styling in plot_ber_read_current_sweep_write_width

--- src/nmem/scripts/plot_ber_read_current_sweep_write_width.py
+++ src/nmem/scripts/plot_ber_read_current_sweep_write_width.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 from nmem.analysis.data_import import import_directory
-from nmem.analysis.styles import apply_global_style
+from nmem.analysis.styles import apply_global_style, apply_legend_style
 from nmem.analysis.sweep_plots import plot_read_sweep_array
 
 # Apply global plot styling
@@ -15,13 +15,7 @@ def main(save_dir=None):
     plot_read_sweep_array(ax, dict_list, "bit_error_rate", "write_width")
     ax.set_xlabel("Read Current [$\\mu$A]")
     ax.set_ylabel("Bit Error Rate")
-    ax.legend(
-        frameon=False,
-        loc="upper left",
-        bbox_to_anchor=(1, 1),
-        title="Write Width",
-    )
-
+    apply_legend_style(ax, title="Write Width [pts]")
     if save_dir:
         plt.savefig(
             f"{save_dir}/ber_read_current_sweep_write_width.png",
